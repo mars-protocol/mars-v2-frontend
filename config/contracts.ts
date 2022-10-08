3580d192de72c8811e1ec5514ee533bb04268b9a
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-08 11:31:24 +0100

    update contract addresses

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,8 +1,8 @@
 // https://github.com/mars-protocol/rover/blob/master/scripts/deploy/addresses/osmo-test-4.json
 export const contractAddresses = {
-  accountNft: 'osmo1xdz3k7dyrjhne749j2ucvy8egrh0rj200r365hdlc4vm6gy0gxsqw9lje4',
-  mockVault: 'osmo1fak59kj9cwh6p3evcvq3q6r0v5q7tpdfxyqsfs48wne28rw9vrjq0x2l3q',
-  marsOracleAdapter: 'osmo108enr4hvmc5w6yum9cx6pnkuudhla4t3se8l0v695y07hfj2zxqsp9sc0j',
-  swapper: 'osmo13qh27g5q5du92fyc0x2cacfgddrl0pzdvd6ulws8tactjdqyxvfqcxe4f9',
-  creditManager: 'osmo13xj9wy673y5g035wlghwfnqdkzawd0gpfc34dm73rns4962fdt9s7yuur7',
+  accountNft: 'osmo1dravtyd0425fkdmkysc3ns7zud05clf5uhj6qqsnkdtrpkewu73q9f3f02',
+  mockVault: 'osmo1emcckulm2mkx36xeanhsn3z3zjeql6pgd8yf8a5cf03ccvy7a4dqjw9tl7',
+  marsOracleAdapter: 'osmo1cw6pv97g7fmhqykrn0gc9ngrx5tnky75rmlwkzxuqhsk58u0n8asz036g0',
+  swapper: 'osmo1w2552km2u9w4k2gjw4n8drmuz5yxw8x4qzy6dl3da824km5cjlys00x3qp',
+  creditManager: 'osmo18dt5y0ecyd5qg8nqwzrgxuljfejglyh2fjd984s8cy7fcx8mxh9qfl3hwq',
 }
