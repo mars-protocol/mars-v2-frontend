16662b417d03532eeedd46455d29643a1ca81168
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-12 22:50:51 +0100

    added icon for atom and tokenInfo data update

--- config/tokenInfo.ts
+++ config/tokenInfo.ts
@@ -2,6 +2,7 @@ type Token = {
   symbol: string
   decimals: number
   icon: string
+  chain: string
 }
 
 const tokenInfo: { [key in string]: Token } = {
@@ -9,11 +10,13 @@ const tokenInfo: { [key in string]: Token } = {
     symbol: 'OSMO',
     decimals: 6,
     icon: '/tokens/osmo.svg',
+    chain: 'Osmosis',
   },
   'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2': {
     symbol: 'ATOM',
-    icon: '',
+    icon: '/tokens/atom.svg',
     decimals: 6,
+    chain: 'Cosmos',
   },
 }
 
