01d3c153fd22af2b42873a8901d25a15b8534515
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-18 17:35:13 +0100

    max borrow amount decimals. memorized return

--- hooks/useCalculateMaxBorrowAmount.tsx
+++ hooks/useCalculateMaxBorrowAmount.tsx
@@ -14,35 +14,42 @@ const useCalculateMaxBorrowAmount = (denom: string, isUnderCollateralized: boole
   const { data: marketsData } = useMarkets()
   const { data: tokenPrices } = useTokenPrices()
 
-  if (!marketsData || !tokenPrices || !positionsData) return 0
+  return useMemo(() => {
+    if (!marketsData || !tokenPrices || !positionsData) return 0
 
-  const getTokenTotalUSDValue = (amount: string, denom: string) => {
-    // early return if prices are not fetched yet
-    if (!tokenPrices) return 0
+    const getTokenTotalUSDValue = (amount: string, denom: string) => {
+      // early return if prices are not fetched yet
+      if (!tokenPrices) return 0
 
-    return BigNumber(amount)
-      .div(10 ** getTokenDecimals(denom))
-      .times(tokenPrices[denom])
-      .toNumber()
-  }
+      return BigNumber(amount)
+        .div(10 ** getTokenDecimals(denom))
+        .times(tokenPrices[denom])
+        .toNumber()
+    }
 
-  const totalWeightedPositions = positionsData?.coins.reduce((acc, coin) => {
-    const tokenWeightedValue = BigNumber(getTokenTotalUSDValue(coin.amount, coin.denom)).times(
-      Number(marketsData[coin.denom].max_loan_to_value)
-    )
+    const totalWeightedPositions = positionsData?.coins.reduce((acc, coin) => {
+      const tokenWeightedValue = BigNumber(getTokenTotalUSDValue(coin.amount, coin.denom)).times(
+        Number(marketsData[coin.denom].max_loan_to_value)
+      )
 
-    return tokenWeightedValue.plus(acc).toNumber()
-  }, 0)
+      return tokenWeightedValue.plus(acc).toNumber()
+    }, 0)
 
-  const borrowTokenPrice = tokenPrices[denom]
+    const borrowTokenPrice = tokenPrices[denom]
+    const tokenDecimals = getTokenDecimals(denom)
 
-  if (isUnderCollateralized) {
-    return BigNumber(totalWeightedPositions)
-      .div(borrowTokenPrice - borrowTokenPrice * Number(marketsData[denom].max_loan_to_value))
-      .toNumber()
-  } else {
-    return BigNumber(totalWeightedPositions).div(borrowTokenPrice).toNumber()
-  }
+    if (isUnderCollateralized) {
+      return BigNumber(totalWeightedPositions)
+        .div(borrowTokenPrice - borrowTokenPrice * Number(marketsData[denom].max_loan_to_value))
+        .decimalPlaces(tokenDecimals)
+        .toNumber()
+    } else {
+      return BigNumber(totalWeightedPositions)
+        .div(borrowTokenPrice)
+        .decimalPlaces(tokenDecimals)
+        .toNumber()
+    }
+  }, [denom, isUnderCollateralized, marketsData, positionsData, tokenPrices])
 }
 
 export default useCalculateMaxBorrowAmount
