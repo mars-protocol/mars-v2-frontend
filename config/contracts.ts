f462675db33acb30742b99c4ce0806666502bd89
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-21 16:23:16 +0100

    update contracts config

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,8 +1,13 @@
 // https://github.com/mars-protocol/rover/blob/master/scripts/deploy/addresses/osmo-test-4.json
-export const contractAddresses = {
+export const roverContracts = {
   accountNft: 'osmo1dravtyd0425fkdmkysc3ns7zud05clf5uhj6qqsnkdtrpkewu73q9f3f02',
   mockVault: 'osmo1emcckulm2mkx36xeanhsn3z3zjeql6pgd8yf8a5cf03ccvy7a4dqjw9tl7',
   marsOracleAdapter: 'osmo1cw6pv97g7fmhqykrn0gc9ngrx5tnky75rmlwkzxuqhsk58u0n8asz036g0',
   swapper: 'osmo1w2552km2u9w4k2gjw4n8drmuz5yxw8x4qzy6dl3da824km5cjlys00x3qp',
   creditManager: 'osmo18dt5y0ecyd5qg8nqwzrgxuljfejglyh2fjd984s8cy7fcx8mxh9qfl3hwq',
+}
+
+export const contractAddresses = {
+  ...roverContracts,
+  redBank: 'osmo1w5rqrdhut890jplmsqnr8gj3uf0wq6lj5rfdnhrtl63lpf6e7v6qalrhhn',
 }
