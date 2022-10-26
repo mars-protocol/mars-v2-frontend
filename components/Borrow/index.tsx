eff6883fe67b9ae49e6987ddfd212f225e3deebc
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-26 15:08:23 +0100

    remove references to deleted components

--- components/Borrow/index.tsx
+++ components/Borrow/index.tsx
@@ -1,3 +1,2 @@
 export { default as AssetRow } from './AssetRow'
-export { default as BorrowFunds } from './BorrowFunds'
-export { default as RepayFunds } from './RepayFunds'
+export { default as BorrowTable } from './BorrowTable'
