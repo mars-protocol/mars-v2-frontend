a9788d6d8057ebde89c705d859391e6920e93243
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-11-07 16:51:09 +0000

    update borrow funds hooks using credit manager client

--- hooks/mutations/useBorrowFunds.tsx
+++ hooks/mutations/useBorrowFunds.tsx
@@ -3,7 +3,6 @@ import { useMemo } from 'react'
 import { toast } from 'react-toastify'
 
 import useWalletStore from 'stores/useWalletStore'
-import { contractAddresses } from 'config/contracts'
 import { hardcodedFee } from 'utils/contants'
 import useCreditManagerStore from 'stores/useCreditManagerStore'
 import { queryKeys } from 'types/query-keys-factory'
@@ -14,61 +13,49 @@ const useBorrowFunds = (
   withdraw = false,
   options: Omit<UseMutationOptions, 'onError'>
 ) => {
-  const signingClient = useWalletStore((s) => s.signingClient)
-  const selectedAccount = useCreditManagerStore((s) => s.selectedAccount)
+  const creditManagerClient = useWalletStore((s) => s.clients.creditManager)
+  const selectedAccount = useCreditManagerStore((s) => s.selectedAccount ?? '')
   const address = useWalletStore((s) => s.address)
 
   const queryClient = useQueryClient()
 
-  const executeMsg = useMemo(() => {
+  const actions = useMemo(() => {
     if (!withdraw) {
-      return {
-        update_credit_account: {
-          account_id: selectedAccount,
-          actions: [
-            {
-              borrow: {
-                denom: denom,
-                amount: String(amount),
-              },
-            },
-          ],
+      return [
+        {
+          borrow: {
+            denom: denom,
+            amount: String(amount),
+          },
         },
-      }
+      ]
     }
 
-    return {
-      update_credit_account: {
-        account_id: selectedAccount,
-        actions: [
-          {
-            borrow: {
-              denom: denom,
-              amount: String(amount),
-            },
-          },
-          {
-            withdraw: {
-              denom: denom,
-              amount: String(amount),
-            },
-          },
-        ],
+    return [
+      {
+        borrow: {
+          denom: denom,
+          amount: String(amount),
+        },
       },
-    }
-  }, [withdraw, selectedAccount, denom, amount])
+      {
+        withdraw: {
+          denom: denom,
+          amount: String(amount),
+        },
+      },
+    ]
+  }, [withdraw, denom, amount])
 
   return useMutation(
     async () =>
-      await signingClient?.execute(
-        address,
-        contractAddresses.creditManager,
-        executeMsg,
+      await creditManagerClient?.updateCreditAccount(
+        { accountId: selectedAccount, actions },
         hardcodedFee
       ),
     {
       onSettled: () => {
-        queryClient.invalidateQueries(queryKeys.creditAccountsPositions(selectedAccount ?? ''))
+        queryClient.invalidateQueries(queryKeys.creditAccountsPositions(selectedAccount))
         queryClient.invalidateQueries(queryKeys.redbankBalances())
 
         // if withdrawing to wallet, need to explicility invalidate balances queries
