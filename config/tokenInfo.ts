c41acb594486e939585e9acee8b9e142e14f1599
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-11-12 18:32:48 +0000

    added CRO to token info

--- config/tokenInfo.ts
+++ config/tokenInfo.ts
@@ -18,6 +18,12 @@ const tokenInfo: { [key in string]: Token } = {
     decimals: 6,
     chain: 'Cosmos',
   },
+  'ibc/E6931F78057F7CC5DA0FD6CEF82FF39373A6E0452BF1FD76910B93292CF356C1': {
+    symbol: 'CRO',
+    icon: '/tokens/cro.jpg',
+    decimals: 8,
+    chain: 'Crypto.org',
+  },
 }
 
 export default tokenInfo
