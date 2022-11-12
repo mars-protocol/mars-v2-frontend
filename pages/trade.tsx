6ae39746a49c3f2dcf1f69d4ea2547b0ad030498
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-11-12 20:17:34 +0000

    trade page initial commit

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -1,17 +1,276 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import { Switch } from '@headlessui/react'
+import BigNumber from 'bignumber.js'
 
 import Container from 'components/Container'
+import Button from 'components/Button'
+import useAllowedCoins from 'hooks/useAllowedCoins'
+import { getTokenDecimals, getTokenSymbol } from 'utils/tokens'
+import Slider from 'components/Slider'
+import useTradeAsset from 'hooks/mutations/useTradeAsset'
+import useAllBalances from 'hooks/useAllBalances'
+import useMarkets from 'hooks/useMarkets'
+import useCreditManagerStore from 'stores/useCreditManagerStore'
+import useCreditAccountPositions from 'hooks/useCreditAccountPositions'
+import useTokenPrices from 'hooks/useTokenPrices'
+
+enum FundingMode {
+  Wallet = 'Wallet',
+  CreditAccount = 'CreditAccount',
+  Both = 'Both',
+}
 
 const Trade = () => {
+  const [selectedTokenIn, setSelectedTokenIn] = useState('')
+  const [selectedTokenOut, setSelectedTokenOut] = useState('')
+  const [amountIn, setAmountIn] = useState(0)
+  const [amountOut, setAmountOut] = useState(0)
+  const [fundingMode, setFundingMode] = useState<FundingMode>(FundingMode.CreditAccount)
+
+  const [isMarginEnabled, setIsMarginEnabled] = React.useState(false)
+
+  const selectedAccount = useCreditManagerStore((s) => s.selectedAccount)
+
+  const { data: allowedCoinsData } = useAllowedCoins()
+  const { data: balancesData } = useAllBalances()
+  const { data: marketsData } = useMarkets()
+  const { data: tokenPrices } = useTokenPrices()
+  const { data: positionsData } = useCreditAccountPositions(selectedAccount ?? '')
+
+  const { mutate } = useTradeAsset(
+    BigNumber(amountIn)
+      .times(10 ** getTokenDecimals(selectedTokenIn))
+      .toNumber(),
+    selectedTokenIn,
+    selectedTokenOut,
+    0.1,
+  )
+
+  useEffect(() => {
+    if (allowedCoinsData && allowedCoinsData.length > 0) {
+      // initialize selected token when allowedCoins fetch data is available
+      setSelectedTokenOut(allowedCoinsData[0])
+
+      if (allowedCoinsData.length > 1) {
+        setSelectedTokenIn(allowedCoinsData[1])
+      } else {
+        setSelectedTokenIn(allowedCoinsData[0])
+      }
+    }
+  }, [allowedCoinsData])
+
+  useEffect(() => {
+    setAmountIn(0)
+    setAmountOut(0)
+  }, [selectedTokenIn, selectedTokenOut, fundingMode])
+
+  const accountAmount = useMemo(() => {
+    return BigNumber(
+      positionsData?.coins?.find((coin) => coin.denom === selectedTokenIn)?.amount ?? 0,
+    )
+      .div(10 ** getTokenDecimals(selectedTokenIn))
+      .toNumber()
+  }, [positionsData, selectedTokenIn])
+
+  const walletAmount = useMemo(() => {
+    return BigNumber(
+      balancesData?.find((balance) => balance.denom === selectedTokenIn)?.amount ?? 0,
+    )
+      .div(10 ** getTokenDecimals(selectedTokenIn))
+      .toNumber()
+  }, [balancesData, selectedTokenIn])
+
+  const maxAmount = useMemo(() => {
+    if (fundingMode === FundingMode.CreditAccount) {
+      return accountAmount
+    }
+
+    if (fundingMode === FundingMode.Wallet) {
+      return walletAmount
+    }
+
+    return accountAmount + walletAmount
+  }, [accountAmount, fundingMode, walletAmount])
+
+  const percentageValue = useMemo(() => {
+    if (isNaN(amountIn) || amountIn === 0) return 0
+
+    return amountIn / maxAmount > 1 ? 100 : (amountIn / maxAmount) * 100
+  }, [amountIn, maxAmount])
+
+  const borrowRate = Number(marketsData?.[selectedTokenOut]?.borrow_rate)
+
+  const handleAmountChange = (value: number, mode: 'in' | 'out') => {
+    const tokenInPrice = tokenPrices?.[selectedTokenIn] ?? 1
+    const tokenOutPrice = tokenPrices?.[selectedTokenOut] ?? 1
+
+    const priceRatio = BigNumber(tokenInPrice).div(tokenOutPrice)
+
+    if (mode === 'in') {
+      setAmountIn(value)
+      setAmountOut(
+        BigNumber(value)
+          .times(priceRatio)
+          .decimalPlaces(getTokenDecimals(selectedTokenOut))
+          .toNumber(),
+      )
+    } else {
+      setAmountOut(value)
+      setAmountIn(
+        BigNumber(1)
+          .div(BigNumber(value).times(priceRatio))
+          .decimalPlaces(getTokenDecimals(selectedTokenIn))
+          .toNumber(),
+      )
+    }
+  }
+
+  const submitDisabled = selectedTokenIn === selectedTokenOut || !amountIn || amountIn > maxAmount
+
   return (
     <div>
       <div className='mb-4 flex gap-4'>
-        <Container className='flex-1'>Graph/Tradingview Module</Container>
+        <Container className='grid flex-1 place-items-center'>Graph/Tradingview Module</Container>
         <div className='flex flex-col gap-4'>
-          <Container>Buy/Sell module</Container>
+          <Container className='!p-2 text-sm'>
+            <div className='border-b border-b-white/20 p-2'>
+              <div className='mb-4 flex gap-4'>
+                <div className='flex-1'>
+                  <p>Buy</p>
+                  <select
+                    className='h-8 w-full text-black'
+                    onChange={(e) => {
+                      setSelectedTokenOut(e.target.value)
+                    }}
+                    value={selectedTokenOut}
+                  >
+                    {allowedCoinsData?.map((entry) => (
+                      <option key={entry} value={entry}>
+                        {getTokenSymbol(entry)}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className='flex-1'>
+                  <p>Sell</p>
+                  <select
+                    className='h-8 w-full text-black'
+                    onChange={(e) => {
+                      setSelectedTokenIn(e.target.value)
+                    }}
+                    value={selectedTokenIn}
+                  >
+                    {allowedCoinsData?.map((entry) => (
+                      <option key={entry} value={entry}>
+                        {getTokenSymbol(entry)}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className='mb-2 flex gap-4'>
+                <div>
+                  <p className='mb-1'>Buy</p>
+                  <input
+                    type='number'
+                    className='px-2 py-1 text-black outline-0'
+                    value={amountOut}
+                    min='0'
+                    onChange={(e) => handleAmountChange(e.target.valueAsNumber, 'out')}
+                    onBlur={(e) => {
+                      if (e.target.value === '') setAmountOut(0)
+                    }}
+                  />
+                </div>
+                <div>
+                  <p className='mb-1'>Sell</p>
+                  <input
+                    type='number'
+                    className='px-2 py-1 text-black outline-0'
+                    value={amountIn}
+                    min='0'
+                    onChange={(e) => handleAmountChange(e.target.valueAsNumber, 'in')}
+                    onBlur={(e) => {
+                      if (e.target.value === '') setAmountIn(0)
+                    }}
+                  />
+                </div>
+              </div>
+              <div className='mb-4'>
+                In Wallet:{' '}
+                {walletAmount.toLocaleString(undefined, {
+                  maximumFractionDigits: getTokenDecimals(selectedTokenIn),
+                })}
+              </div>
+              <div className='mb-4'>
+                In Account:{' '}
+                {accountAmount.toLocaleString(undefined, {
+                  maximumFractionDigits: getTokenDecimals(selectedTokenIn),
+                })}
+              </div>
+              <Slider
+                className='mb-6'
+                value={percentageValue}
+                onChange={(value) => {
+                  const decimal = value[0] / 100
+                  const tokenDecimals = getTokenDecimals(selectedTokenIn)
+                  // limit decimal precision based on token contract decimals
+                  const newAmount = Number((decimal * maxAmount).toFixed(tokenDecimals))
+
+                  handleAmountChange(newAmount, 'in')
+                }}
+                onMaxClick={() => setAmountIn(maxAmount)}
+              />
+            </div>
+            <div className='border-b border-b-white/20 p-2'>
+              <div className='mb-2 flex items-center'>
+                <p className='mr-2'>Margin</p>
+                <Switch
+                  checked={isMarginEnabled}
+                  onChange={setIsMarginEnabled}
+                  className={`${
+                    isMarginEnabled ? 'bg-[#524BB1]' : 'bg-gray-400'
+                  } relative inline-flex h-4 w-8 items-center rounded-full`}
+                >
+                  <span
+                    className={`${
+                      isMarginEnabled ? 'translate-x-4' : ''
+                    } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                  />
+                </Switch>
+              </div>
+              <div className='mb-2 flex justify-between'>
+                <p>Borrow</p>
+                <p>{isMarginEnabled ? 'BORROW VALUE' : '-'}</p>
+              </div>
+              <div className='flex justify-between'>
+                <p>Borrow Rate</p>
+                <p>{isMarginEnabled ? `${(borrowRate * 100).toFixed(2)}%` : '-'}</p>
+              </div>
+            </div>
+            <div className='h-[100px] p-2'>
+              <div className='mb-6'>OTHER INFO PLACEHOLDER</div>
+              <div className='flex justify-between'>
+                <p>Funded From</p>
+                <select
+                  value={fundingMode}
+                  className='text-black'
+                  onChange={(e) => {
+                    setFundingMode(e.target.value as FundingMode)
+                  }}
+                >
+                  <option value={FundingMode.CreditAccount}>Account</option>
+                  <option value={FundingMode.Wallet}>Wallet</option>
+                  <option value={FundingMode.Both}>Wallet & Account</option>
+                </select>
+              </div>
+            </div>
+            <Button className='w-full' onClick={() => mutate()} disabled={submitDisabled}>
+              Create Order
+            </Button>
+          </Container>
           <Container>Orderbook module (optional)</Container>
         </div>
-        <Container>Credit Account essential module</Container>
       </div>
       <Container>Trader order overview</Container>
     </div>
