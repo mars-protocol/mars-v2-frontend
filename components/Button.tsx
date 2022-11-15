d042b6295b0273359835cd5a8d7f18a3eb3314c3
Linkie Link <linkielink.dev@gmail.com>
2022-11-15 16:07:20 +0100

    MP-1669: added Link component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,113 +4,88 @@ import React, { ReactNode } from 'react'
 import CircularProgress from './CircularProgress'
 
 interface Props {
+  children?: string | ReactNode
   className?: string
   color?: 'primary' | 'secondary' | 'tertiary' | 'quaternary'
   disabled?: boolean
-  externalLink?: string
   id?: string
-  children?: string | ReactNode
-  text?: string | ReactNode
   showProgressIndicator?: boolean
   size?: 'small' | 'medium' | 'large'
+  text?: string | ReactNode
   variant?: 'solid' | 'transparent' | 'round'
   onClick?: (e: any) => void
 }
 
-const Button = React.forwardRef<any, Props>(
-  (
-    {
-      children,
-      className = '',
-      color = 'primary',
-      disabled,
-      externalLink,
-      id = '',
-      showProgressIndicator,
-      size = 'small',
-      text,
-      variant = 'solid',
-      onClick,
-    },
-    ref,
-  ) => {
-    const Button = () => {
-      const colorClasses = {
-        primary:
-          'border-none bg-primary hover:bg-primary-highlight active:bg-primary-highlight-10 focus:bg-primary-highlight',
-        secondary:
-          'border-none bg-secondary hover:bg-secondary-highlight active:bg-secondary-highlight-10 focus:bg-secondary-highlight',
-        tertiary:
-          'border bg-secondary-dark/60 border-white/60 hover:bg-secondary-dark hover:border-white active:bg-secondary-dark-10 active:border-white focus:bg-secondary-dark focus:border-white',
-        quaternary:
-          'border bg-transparent text-white/60 border-transparent hover:text-white hover:border-white active:text-white active:border-white',
-      }
+const Button = ({
+  children,
+  className = '',
+  color = 'primary',
+  disabled,
+  id = '',
+  showProgressIndicator,
+  size = 'small',
+  text,
+  variant = 'solid',
+  onClick,
+}: Props) => {
+  const colorClasses = {
+    primary:
+      'border-none bg-primary hover:bg-primary-highlight active:bg-primary-highlight-10 focus:bg-primary-highlight',
+    secondary:
+      'border-none bg-secondary hover:bg-secondary-highlight active:bg-secondary-highlight-10 focus:bg-secondary-highlight',
+    tertiary:
+      'border bg-secondary-dark/60 border-white/60 hover:bg-secondary-dark hover:border-white active:bg-secondary-dark-10 active:border-white focus:bg-secondary-dark focus:border-white',
+    quaternary:
+      'border bg-transparent text-white/60 border-transparent hover:text-white hover:border-white active:text-white active:border-white',
+  }
 
-      const transparentColorClasses = {
-        primary:
-          'border-none text-primary hover:text-primary-highlight active:text-primary-highlight focus:text-primary-highlight',
-        secondary:
-          'border-none text-secondary hover:text-secondary-highlight active:text-secondary-highlight focus:text-secondary-highlight',
-        tertiary:
-          'text-secondary-dark hover:text-secondary-dark-10 active:text-secondary-dark-10 focus:text-secondary-dark-10',
-        quaternary:
-          'border border-transparent text-white/60 hover:text-white hover:border-white active:text-white active:border-white',
-      }
+  const transparentColorClasses = {
+    primary:
+      'border-none text-primary hover:text-primary-highlight active:text-primary-highlight focus:text-primary-highlight',
+    secondary:
+      'border-none text-secondary hover:text-secondary-highlight active:text-secondary-highlight focus:text-secondary-highlight',
+    tertiary:
+      'text-secondary-dark hover:text-secondary-dark-10 active:text-secondary-dark-10 focus:text-secondary-dark-10',
+    quaternary:
+      'border border-transparent text-white/60 hover:text-white hover:border-white active:text-white active:border-white',
+  }
 
-      const roundSizeClasses = {
-        small: 'h-[32px] w-[32px]',
-        medium: 'h-[40px] w-[40px]',
-        large: 'h-[56px] w-[56px]',
-      }
+  const roundSizeClasses = {
+    small: 'h-[32px] w-[32px]',
+    medium: 'h-[40px] w-[40px]',
+    large: 'h-[56px] w-[56px]',
+  }
 
-      const sizeClasses = {
-        small: 'text-sm px-5 py-1.5 min-h-[32px]',
-        medium: 'text-base px-6 py-2.5 min-h-[40px]',
-        large: 'text-lg px-6 py-2.5 min-h-[56px]',
-      }
+  const sizeClasses = {
+    small: 'text-sm px-5 py-1.5 min-h-[32px]',
+    medium: 'text-base px-6 py-2.5 min-h-[40px]',
+    large: 'text-lg px-6 py-2.5 min-h-[56px]',
+  }
 
-      const variantClasses = {
-        solid: 'text-white',
-        transparent: 'bg-transparent p-0',
-        round: 'rounded-full p-0',
-      }
+  const variantClasses = {
+    solid: 'text-white',
+    transparent: 'bg-transparent p-0',
+    round: 'rounded-full p-0',
+  }
 
-      const buttonClasses = classNames(
-        'transition-colors appearance-none rounded-3xl cursor-pointer outline-none break-normal',
-        variant === 'round' ? `${sizeClasses[size]} ${roundSizeClasses[size]}` : sizeClasses[size],
-        variant === 'transparent' ? transparentColorClasses[color] : colorClasses[color],
-        variantClasses[variant],
-        disabled && 'pointer-events-none opacity-50',
-        className,
-      )
-      return (
-        <button className={buttonClasses} id={id} onClick={disabled ? () => {} : onClick} ref={ref}>
-          <>
-            {text && !children && !showProgressIndicator && <span>{text}</span>}
-            {children && !showProgressIndicator && children}
-            {showProgressIndicator && (
-              <CircularProgress size={size === 'small' ? 10 : size === 'medium' ? 12 : 18} />
-            )}
-          </>
-        </button>
-      )
-    }
+  const buttonClasses = classNames(
+    'transition-colors appearance-none rounded-3xl cursor-pointer outline-none break-normal',
+    variant === 'round' ? `${sizeClasses[size]} ${roundSizeClasses[size]}` : sizeClasses[size],
+    variant === 'transparent' ? transparentColorClasses[color] : colorClasses[color],
+    variantClasses[variant],
+    disabled && 'pointer-events-none opacity-50',
+    className,
+  )
 
-    return externalLink ? (
-      <a
-        className='flex hover:no-underline focus:no-underline active:no-underline'
-        href={externalLink}
-        ref={ref}
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        {Button()}
-      </a>
-    ) : (
-      Button()
-    )
-  },
-)
+  return (
+    <button className={buttonClasses} id={id} onClick={disabled ? () => {} : onClick}>
+      {text && !children && !showProgressIndicator && <span>{text}</span>}
+      {children && !showProgressIndicator && children}
+      {showProgressIndicator && (
+        <CircularProgress size={size === 'small' ? 10 : size === 'medium' ? 12 : 18} />
+      )}
+    </button>
+  )
+}
 
-Button.displayName = 'Button'
 export default Button
---
+++ components/Link.tsx
@@ -0,0 +1,76 @@
+import { StringOrTemplateHeader } from '@tanstack/react-table'
+import classNames from 'classnames'
+import React, { ReactNode } from 'react'
+
+import CircularProgress from './CircularProgress'
+
+interface Props {
+  children?: string | ReactNode
+  className?: string
+  color?: 'primary' | 'secondary' | 'tertiary' | 'quaternary'
+  disabled?: boolean
+  externalLink?: String
+  href?: string
+  size?: 'small' | 'medium' | 'large'
+  text?: string | ReactNode
+  onClick?: (e: any) => void
+}
+
+const Link = ({
+  children,
+  className = '',
+  color = 'primary',
+  disabled,
+  externalLink,
+  href,
+  size = 'small',
+  text,
+  onClick,
+}: Props) => {
+  const Link = () => {
+    const colorClasses = {
+      primary:
+        'text-primary hover:text-primary-highlight active:text-primary-highlight-10 focus:text-primary-highlight',
+      secondary:
+        'text-secondary hover:text-secondary-highlight active:text-secondary-highlight-10 focus:text-secondary-highlight',
+      tertiary:
+        'text-secondary-dark/60 hover:text-secondary-dark active:text-secondary-dark-10 focus:text-secondary-dark',
+      quaternary: 'text-transparent text-white/60 hover:text-white active:text-white',
+    }
+    const sizeClasses = {
+      small: 'text-sm',
+      medium: 'text-base',
+      large: 'text-lg',
+    }
+
+    const linkClasses = classNames(
+      sizeClasses[size],
+      colorClasses[color],
+      disabled && 'pointer-events-none opacity-50',
+      className,
+    )
+
+    return (
+      <a
+        className={linkClasses}
+        target={externalLink ? '_blank' : '_self'}
+        rel='noreferrer'
+        onClick={
+          onClick && href
+            ? (e) => {
+                e.preventDefault()
+                if (disabled) return
+                onClick
+              }
+            : undefined
+        }
+        href={!href ? '#' : href}
+      >
+        {text && !children && text}
+        {children && children}
+      </a>
+    )
+  }
+}
+
+export default Link
