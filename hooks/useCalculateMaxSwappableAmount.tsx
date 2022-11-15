37adf6b4dfc5e7119be51b6196d595dd90bddeb9
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-11-15 09:36:13 +0000

    max swap amount formula adjustments

--- hooks/useCalculateMaxSwappableAmount.tsx
+++ hooks/useCalculateMaxSwappableAmount.tsx
@@ -1,63 +1,125 @@
-import React, { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import BigNumber from 'bignumber.js'
 
 import { getTokenDecimals } from 'utils/tokens'
 import useCreditManagerStore from 'stores/useCreditManagerStore'
+import { chain } from 'utils/chains'
 
 import useCreditAccountPositions from './useCreditAccountPositions'
 import useMarkets from './useMarkets'
 import useRedbankBalances from './useRedbankBalances'
 import useTokenPrices from './useTokenPrices'
-import useAllBalances from './useAllBalances'
 
-enum FundingMode {
-  Wallet = 'Wallet',
-  CreditAccount = 'CreditAccount',
-  Both = 'Both',
-}
-
-const useCalculateMaxSwappableAmount = (
-  tokenIn: string,
-  tokenOut: string,
-  fundingFrom: FundingMode,
-) => {
+// max swap amount doesnt consider wallet balance as its not relevant
+// the entire token balance within the wallet will always be able to be fully swapped
+const useCalculateMaxSwappableAmount = (tokenIn: string, tokenOut: string) => {
   const selectedAccount = useCreditManagerStore((s) => s.selectedAccount)
 
-  const { data: walletBalancesData } = useAllBalances()
   const { data: positionsData } = useCreditAccountPositions(selectedAccount ?? '')
   const { data: marketsData } = useMarkets()
   const { data: tokenPrices } = useTokenPrices()
   const { data: redbankBalances } = useRedbankBalances()
 
-  const { accountAmount, walletAmount } = useMemo(() => {
-    const accountAmount = BigNumber(
+  const accountAmount = useMemo(() => {
+    return BigNumber(
       positionsData?.coins?.find((coin) => coin.denom === tokenIn)?.amount ?? 0,
-    )
-      .div(10 ** getTokenDecimals(tokenIn))
+    ).toNumber()
+  }, [positionsData, tokenIn])
+
+  const getTokenValue = useCallback(
+    (amount: string, denom: string) => {
+      if (!tokenPrices) return 0
+
+      return BigNumber(amount).times(tokenPrices[denom]).toNumber()
+    },
+    [tokenPrices],
+  )
+
+  return useMemo(() => {
+    if (!marketsData || !tokenPrices || !positionsData || !redbankBalances || !tokenIn || !tokenOut)
+      return 0
+
+    const totalWeightedPositions = positionsData?.coins.reduce((acc, coin) => {
+      const tokenWeightedValue = BigNumber(getTokenValue(coin.amount, coin.denom)).times(
+        Number(marketsData[coin.denom].max_loan_to_value),
+      )
+
+      return tokenWeightedValue.plus(acc).toNumber()
+    }, 0)
+
+    const totalLiabilitiesValue = positionsData?.debts.reduce((acc, coin) => {
+      const tokenDebtValue = BigNumber(getTokenValue(coin.amount, coin.denom))
+
+      return tokenDebtValue.plus(acc).toNumber()
+    }, 0)
+
+    const tokenOutLTV = Number(marketsData[tokenOut].max_loan_to_value)
+    const tokenInLTV = Number(marketsData[tokenIn].max_loan_to_value)
+
+    // in theory, the most you can swap from x to y while keeping an health factor of 1
+    const maxSwapValue = BigNumber(totalLiabilitiesValue)
+      .minus(totalWeightedPositions)
+      .dividedBy(tokenOutLTV - tokenInLTV)
+      .decimalPlaces(0)
       .toNumber()
 
-    const walletAmount = BigNumber(
-      walletBalancesData?.find((balance) => balance.denom === tokenIn)?.amount ?? 0,
-    )
-      .div(10 ** getTokenDecimals(tokenIn))
+    const maxSwapAmount = BigNumber(maxSwapValue)
+      .div(tokenPrices[tokenIn])
+      .decimalPlaces(0)
       .toNumber()
 
-    return { accountAmount, walletAmount }
-  }, [walletBalancesData, positionsData, tokenIn])
-
-  const maxAmount = useMemo(() => {
-    if (fundingFrom === FundingMode.CreditAccount) {
-      return accountAmount
+    // if the target token ltv higher, the full amount will always be able to be swapped
+    if (tokenOutLTV < tokenInLTV) {
+      // if the swappable amount is lower than the account amount, any further calculations are irrelevant
+      if (BigNumber(maxSwapAmount).isLessThanOrEqualTo(accountAmount))
+        return BigNumber(maxSwapAmount)
+          .dividedBy(10 ** getTokenDecimals(tokenIn))
+          .toNumber()
     }
 
-    if (fundingFrom === FundingMode.Wallet) {
-      return walletAmount
-    }
+    const estimatedTokenOutAmount = BigNumber(accountAmount).times(
+      tokenPrices[tokenIn] / tokenPrices[tokenOut],
+    )
 
-    return accountAmount + walletAmount
-  }, [accountAmount, fundingFrom, walletAmount])
+    // calculate weighted positions assuming the initial swap is made
+    const totalWeightedPositionsAfterSwap = positionsData?.coins
+      .filter((coin) => coin.denom !== tokenIn)
+      .reduce((acc, coin) => {
+        const coinAmount =
+          coin.denom === tokenOut
+            ? BigNumber(coin.amount).plus(estimatedTokenOutAmount).toString()
+            : coin.amount
 
-  return maxAmount
+        const tokenWeightedValue = BigNumber(getTokenValue(coinAmount, coin.denom)).times(
+          Number(marketsData[coin.denom].max_loan_to_value),
+        )
+
+        return tokenWeightedValue.plus(acc).toNumber()
+      }, 0)
+
+    const maxBorrowValue = BigNumber(totalWeightedPositionsAfterSwap)
+      .minus(totalLiabilitiesValue)
+      .dividedBy(1 - tokenOutLTV)
+
+    const maxBorrowAmount = maxBorrowValue
+      .dividedBy(tokenPrices[tokenIn])
+      .decimalPlaces(getTokenDecimals(tokenIn))
+      .toNumber()
+
+    return BigNumber(accountAmount)
+      .plus(maxBorrowAmount)
+      .dividedBy(10 ** getTokenDecimals(tokenIn))
+      .toNumber()
+  }, [
+    accountAmount,
+    getTokenValue,
+    marketsData,
+    positionsData,
+    redbankBalances,
+    tokenIn,
+    tokenOut,
+    tokenPrices,
+  ])
 }
 
 export default useCalculateMaxSwappableAmount
