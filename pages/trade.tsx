d30f376d3e32d57ebead0440a664333ca9b1c2ed
Linkie Link <linkielink.dev@gmail.com>
2022-11-30 14:47:31 +0100

    tidy: cleaning up the trading view

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -1,22 +1,37 @@
-import React from 'react'
-
 import Card from 'components/Card'
+import Text from 'components/Text'
 import TradeActionModule from 'components/Trade/TradeActionModule'
 
 const Trade = () => {
   return (
     <div>
       <div className='mb-4 flex gap-4'>
-        <Card className='flex-1'>Graph/Tradingview Module</Card>
+        <Card className='flex-1'>
+          <Text size='lg' uppercase={true}>
+            Tradingview Graph
+          </Text>
+        </Card>
         <div className='flex flex-col gap-4'>
           <Card>
             <TradeActionModule />
           </Card>
-          <Card>Orderbook module (optional)</Card>
+          <Card>
+            <Text size='lg' uppercase={true}>
+              Orderbook module (optional)
+            </Text>
+          </Card>
         </div>
-        <Card>Credit Account essential module</Card>
+        <Card className='flex w-[374px]'>
+          <Text size='lg' uppercase={true}>
+            Credit Account Stats
+          </Text>
+        </Card>
       </div>
-      <Card>Trader order overview</Card>
+      <Card>
+        <Text size='lg' uppercase={true}>
+          Order history
+        </Text>
+      </Card>
     </div>
   )
 }
