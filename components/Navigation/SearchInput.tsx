0eb3c96941fe58b160bcdfebde8dcfb1e07ea6dc
Linkie Link <linkielink.dev@gmail.com>
2022-12-01 09:57:59 +0100

    MP-1758: finished the SearchInput layout

--- components/Navigation/SearchInput.tsx
+++ components/Navigation/SearchInput.tsx
@@ -1,20 +1,11 @@
+import SearchIcon from 'components/Icons/search.svg'
 const SearchInput = () => (
   <div className='relative mt-[1px] py-2 text-white'>
-    <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
-      <svg
-        fill='none'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        viewBox='0 0 24 24'
-        className='h-6 w-6'
-      >
-        <path d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
-      </svg>
+    <span className='absolute top-1/2 left-0 flex h-6 w-8 -translate-y-1/2 items-center pl-2'>
+      <SearchIcon />
     </span>
     <input
-      className='rounded-md bg-black/70 py-2 pl-10 text-sm text-white focus:outline-none'
+      className='w-[280px] rounded-md border border-white/20 bg-black/30 py-2 pl-10 text-sm text-white placeholder:text-white/40 focus:outline-none'
       placeholder='Search'
     />
   </div>
