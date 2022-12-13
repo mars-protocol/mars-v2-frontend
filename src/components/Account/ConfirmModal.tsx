d3a9da2456fea942590024b4e3d3b4c98763477f
Linkie Link <linkielink.dev@gmail.com>
2022-12-13 09:46:16 +0100

    fix: change order of the elements

--- src/components/Account/ConfirmModal.tsx
+++ src/components/Account/ConfirmModal.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+
 import { CircularProgress, Modal, Text } from 'components'
 import { MarsProtocol } from 'components/Icons'
 import { useModalStore } from 'stores'
@@ -17,14 +18,14 @@ export const ConfirmModal = () => {
         )}
       >
         <div className='w-full flex-wrap'>
-          <Text size='2xl' uppercase={true} className='mb-6 w-full text-center'>
+          <div className='flex w-full justify-center pb-6'>
+            <CircularProgress size={40} />
+          </div>
+          <Text size='2xl' uppercase={true} className='w-full text-center'>
             {createOpen &&
               'A small step for a Smart Contracts but a big leap for your financial freedom.'}
             {deleteOpen && 'Some rovers have to be recycled once in a while...'}
           </Text>
-          <div className='flex w-full justify-center pb-6'>
-            <CircularProgress size={40} />
-          </div>
         </div>
         <div className='absolute bottom-8 left-8 flex w-[150px]'>
           <MarsProtocol />
