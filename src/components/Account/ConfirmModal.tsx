fa2e93b6baa8d47d6b391a98c802c764ba887356
Linkie Link <linkielink.dev@gmail.com>
2022-12-13 09:45:03 +0100

    feat: added nice create and delete modals

--- src/components/Account/ConfirmModal.tsx
+++ src/components/Account/ConfirmModal.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames'
 import { CircularProgress, Modal, Text } from 'components'
+import { MarsProtocol } from 'components/Icons'
 import { useModalStore } from 'stores'
 
 export const ConfirmModal = () => {
@@ -7,12 +9,25 @@ export const ConfirmModal = () => {
 
   return (
     <Modal open={createOpen || deleteOpen}>
-      <div className='w-full p-6'>
-        <Text size='2xl' uppercase={true} className='mb-6 w-full text-center'>
-          Confirm Transaction
-        </Text>
-        <div className='flex w-full justify-center pb-6'>
-          <CircularProgress size={40} />
+      <div
+        className={classNames(
+          'relative flex h-[630px] w-full flex-wrap items-center justify-center p-6',
+          createOpen && 'bg-create-modal',
+          deleteOpen && 'bg-delete-modal',
+        )}
+      >
+        <div className='w-full flex-wrap'>
+          <Text size='2xl' uppercase={true} className='mb-6 w-full text-center'>
+            {createOpen &&
+              'A small step for a Smart Contracts but a big leap for your financial freedom.'}
+            {deleteOpen && 'Some rovers have to be recycled once in a while...'}
+          </Text>
+          <div className='flex w-full justify-center pb-6'>
+            <CircularProgress size={40} />
+          </div>
+        </div>
+        <div className='absolute bottom-8 left-8 flex w-[150px]'>
+          <MarsProtocol />
         </div>
       </div>
     </Modal>
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,9 @@ module.exports = {
       },
       backgroundImage: {
         mars: 'url(/images/bg.svg)',
-        'fund-modal': 'url(/images/fund-modal-bg.png)',
+        'fund-modal': 'url(/images/fund-bg.webp), url(/images/fund-bg.png)',
+        'delete-modal': 'url(/images/delete-account-bg.webp), url(/images/delete-account-bg.png)',
+        'create-modal': 'url(/images/create-account-bg.webp), url(/images/create-account-bg.png)',
       },
       backgroundSize: {
         desktop: '100% auto',
