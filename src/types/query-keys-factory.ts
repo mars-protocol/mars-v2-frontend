a03691ad9237a066a6386429daeca1f8db8d0c57
Linkie Link <linkielink.dev@gmail.com>
2023-02-20 13:19:10 +0100

    MP-2258: added estimateFee hook (#94)

--- src/types/query-keys-factory.ts
+++ src/types/query-keys-factory.ts
@@ -1,9 +1,10 @@
 export const queryKeys = {
   allBalances: (address: string) => ['allBalances', address],
   allowedCoins: () => ['allowedCoins'],
-  redbankBalances: () => ['redbankBalances'],
+  estimateFee: () => ['estimateFee'],
   creditAccounts: (address: string) => ['creditAccounts', address],
   creditAccountsPositions: (accountId: string) => ['creditAccountPositions', accountId],
+  redbankBalances: () => ['redbankBalances'],
   tokenBalance: (address: string, denom: string) => ['tokenBalance', address, denom],
   tokenPrices: () => ['tokenPrices'],
 }
