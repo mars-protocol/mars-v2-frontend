1ea0f130ca02d12b053470ef6290a308210a058a
Linkie Link <linkielink.dev@gmail.com>
2023-03-08 16:17:12 +0100

    env: moved cors back to vercel.json

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ const nextConfig = {
     appDir: true,
   },
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: '*' },
-        ],
-      },
-    ]
-  },
   async redirects() {
     return [
       {
