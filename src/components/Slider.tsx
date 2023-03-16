13e88ea062541df37be415f6f195927ac6966137
bwvdhelm <34470358+bobthebuidlr@users.noreply.github.com>
2023-03-16 10:35:56 +0100

    :bento: Add Slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,36 +1,162 @@
-import * as RadixSlider from '@radix-ui/react-slider'
+import { ChangeEvent, useRef, useState } from 'react'
+import Draggable from 'react-draggable'
+
+import { SliderMark } from 'components/Icons/index'
 
 type Props = {
-  className?: string
   value: number
-  onChange: (value: number[]) => void
-  onMaxClick: () => void
+  onChange: (value: number) => void
 }
 
-export const Slider = ({ className, value, onChange, onMaxClick }: Props) => {
+export default function Slider(props: Props) {
+  const [showTooltip, setShowTooltip] = useState(false)
+  const [sliderRect, setSliderRect] = useState({ width: 0, left: 0, right: 0 })
+  const ref = useRef<HTMLDivElement>(null)
+  const nodeRef = useRef(null)
+  const [isDragging, setIsDragging] = useState(false)
+
+  function handleSliderRect() {
+    const leftCap = ref.current?.getBoundingClientRect().left ?? 0
+    const rightCap = ref.current?.getBoundingClientRect().right ?? 0
+    const newSliderWidth = ref.current?.getBoundingClientRect().width ?? 0
+
+    if (
+      sliderRect.width !== newSliderWidth ||
+      leftCap !== sliderRect.left ||
+      rightCap !== sliderRect.right
+    ) {
+      setSliderRect({
+        left: leftCap,
+        right: rightCap,
+        width: newSliderWidth,
+      })
+    }
+  }
+
+  function handleDrag(e: any) {
+    if (!isDragging) {
+      setIsDragging(true)
+    }
+
+    const current: number = e.clientX
+
+    if (current < sliderRect.left) {
+      props.onChange(0)
+      return
+    }
+
+    if (current > sliderRect.right) {
+      props.onChange(100)
+      return
+    }
+
+    const value = Math.round(((current - sliderRect.left) / sliderRect.width) * 100)
+
+    if (value !== props.value) {
+      props.onChange(value)
+    }
+  }
+
+  function handleSliderClick(e: ChangeEvent<HTMLInputElement>) {
+    props.onChange(Number(e.target.value))
+  }
+
+  function handleShowTooltip() {
+    setShowTooltip(true)
+  }
+
+  function handleHideTooltip() {
+    setShowTooltip(false)
+  }
+
   return (
-    <div className={`relative flex flex-1 items-center ${className || ''}`}>
-      <RadixSlider.Root
-        className='relative flex h-[20px] w-full cursor-pointer touch-none select-none items-center'
-        value={[value]}
-        min={0}
-        max={100}
-        step={1}
-        onValueChange={(value) => onChange(value)}
-      >
-        <RadixSlider.Track className='relative h-[6px] grow rounded-full bg-gray-400'>
-          <RadixSlider.Range className='absolute h-[100%] rounded-full bg-blue-600' />
-        </RadixSlider.Track>
-        <RadixSlider.Thumb className='flex h-[20px] w-[20px] items-center justify-center rounded-full bg-white !outline-none'>
-          <div className='relative top-5 text-xs'>{value.toFixed(0)}%</div>
-        </RadixSlider.Thumb>
-      </RadixSlider.Root>
-      <button
-        className='ml-4 rounded-base bg-blue-600 py-1 px-2 text-xs font-semibold text-white'
-        onClick={onMaxClick}
-      >
-        MAX
-      </button>
+    <div>
+      <div ref={ref} className='relative w-[446px]' onMouseEnter={handleSliderRect}>
+        <input
+          type='range'
+          value={props.value}
+          onChange={handleSliderClick}
+          onMouseDown={handleShowTooltip}
+          className='absolute z-2 w-[100%] cursor-pointer appearance-none bg-transparent [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:appearance-none'
+        />
+        <div className='absolute flex  w-[100%] items-center gap-1'>
+          <Mark onClick={props.onChange} value={0} sliderValue={props.value} />
+          <Track maxValue={23} sliderValue={props.value} />
+          <Mark onClick={props.onChange} value={25} sliderValue={props.value} />
+          <Track maxValue={48} sliderValue={props.value} />
+          <Mark onClick={props.onChange} value={50} sliderValue={props.value} />
+          <Track maxValue={73} sliderValue={props.value} />
+          <Mark onClick={props.onChange} value={75} sliderValue={props.value} />
+          <Track maxValue={98} sliderValue={props.value} />
+          <Mark onClick={props.onChange} value={100} sliderValue={props.value} />
+        </div>
+        <div onMouseEnter={handleShowTooltip} onMouseLeave={handleHideTooltip}>
+          <Draggable
+            nodeRef={nodeRef}
+            axis='x'
+            grid={[sliderRect.width / 100, 0]}
+            bounds={{ left: 0, right: sliderRect.width }}
+            positionOffset={{ x: (props.value / 100) * -12, y: 0 }}
+            onDrag={handleDrag}
+            onStop={() => setIsDragging(false)}
+            position={{ x: (sliderRect.width / 100) * props.value, y: 0 }}
+          >
+            <div ref={nodeRef} className='absolute z-20 leading-3'>
+              <div
+                className={
+                  'z-20 h-3 w-3 rotate-45 cursor-pointer rounded-xs border-[2px] border-white bg-martian-red'
+                }
+              ></div>
+              {(showTooltip || isDragging) && (
+                <div className='absolute top-[-32px] left-[50%] translate-x-[-50%] rounded-xs bg-martian-red py-[2px] px-2 text-xs'>
+                  <SliderMark className='absolute left-[50%] bottom-[-4px] translate-x-[-50%]' />
+                  {props.value}%
+                </div>
+              )}
+            </div>
+          </Draggable>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+interface MarkProps {
+  value: number
+  sliderValue: number
+  onClick: (value: number) => void
+}
+
+function Mark(props: MarkProps) {
+  return (
+    <button
+      onClick={() => props.onClick(props.value)}
+      className={`z-20 h-3 w-3 rotate-45 rounded-xs border-[1px] border-white/20 hover:border-[2px] hover:border-white ${
+        props.sliderValue >= props.value ? 'bg-martian-red hover:border-white' : 'bg-grey-medium'
+      }`}
+    ></button>
+  )
+}
+
+interface TrackProps {
+  maxValue: number
+  sliderValue: number
+}
+
+function Track(props: TrackProps) {
+  const minValue = props.maxValue - 21
+  let percentage = 0
+
+  if (props.sliderValue >= props.maxValue) percentage = 100
+
+  if (props.sliderValue > minValue && props.sliderValue < props.maxValue) {
+    percentage = ((props.sliderValue - minValue) / (props.maxValue - minValue)) * 100
+  }
+
+  return (
+    <div className='relative h-1 flex-grow overflow-hidden rounded-sm bg-transparent'>
+      <div className='absolute z-1 h-3 bg-martian-red ' style={{ width: `${percentage}%` }}></div>
+      <div className='absolute h-3 w-[100%] bg-white/20'></div>
     </div>
   )
 }
