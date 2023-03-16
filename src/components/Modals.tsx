aaa57af055c4e46b25ce3d5d273767cff9926255
bwvdhelm <34470358+bobthebuidlr@users.noreply.github.com>
2023-03-16 10:47:09 +0100

    :fire: remove unused components

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { ConfirmModal } from 'components/Account/ConfirmModal'
-import { FundAccountModal } from 'components/Account/FundAccountModal'
 import BorrowModal from 'components/BorrowModal'
 
 export const Modals = () => (
   <>
-    <FundAccountModal />
-    {/* <WithdrawModal /> */}
     <ConfirmModal />
     <BorrowModal />
   </>
