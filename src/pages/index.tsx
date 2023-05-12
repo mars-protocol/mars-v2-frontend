affd2190ca6ed65e70544c6d6298982cf5954c18
Yusuf Seyrek <yusuf@delphilabs.io>
2023-05-12 19:17:45 +0300

    feat: route changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 export default function App() {
   return (
     <Router>
-      <div>
-        Test App
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path='/about' element={<h1>About</h1>} />
-          <Route path='/' element={<h1>Home</h1>} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/trade' element={<h1>Trade Component will be rendered here!</h1>} />
+        <Route path='/council' element={<h1>Council Component will be rendered here!</h1>} />
+        <Route path='/' element={<h1>Home</h1>} />
+      </Routes>
     </Router>
   )
 }
