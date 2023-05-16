d2142c280076d4e7cd5ff784812718fa0458a2af
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-05-16 12:35:22 +0200

    Fix routing and wallet client (#205)
    * Add header to router (as layout)
    
    * Refactor Wallet component
    
    * Remove server fallback packages webpack
    
    * add missing dependency for useeffect

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -6,19 +6,35 @@ import FarmPage from 'pages/FarmPage'
 import LendPage from 'pages/LendPage'
 import PortfolioPage from 'pages/PortfolioPage'
 import TradePage from 'pages/TradePage'
+import Layout from 'pages/_layout'
 
 export default function Routes() {
   return (
     <RoutesWrapper>
-      <Route path='/trade' element={<TradePage />} />
-      <Route path='/farm' element={<FarmPage />} />
-      <Route path='/lend' element={<LendPage />} />
-      <Route path='/borrow' element={<BorrowPage />} />
-      <Route path='/portfolio' element={<PortfolioPage />} />
-      <Route path='/council' element={<CouncilPage />} />
-      <Route path='/' element={<TradePage />} />
-      <Route path='/wallets/:address'>
-        <Route path='accounts/:accountId'>
+      <Route
+        element={
+          <Layout>
+            <Outlet />
+          </Layout>
+        }
+      >
+        <Route path='/trade' element={<TradePage />} />
+        <Route path='/farm' element={<FarmPage />} />
+        <Route path='/lend' element={<LendPage />} />
+        <Route path='/borrow' element={<BorrowPage />} />
+        <Route path='/portfolio' element={<PortfolioPage />} />
+        <Route path='/council' element={<CouncilPage />} />
+        <Route path='/' element={<TradePage />} />
+        <Route path='/wallets/:address'>
+          <Route path='accounts/:accountId'>
+            <Route path='trade' element={<TradePage />} />
+            <Route path='farm' element={<FarmPage />} />
+            <Route path='lend' element={<LendPage />} />
+            <Route path='borrow' element={<BorrowPage />} />
+            <Route path='portfolio' element={<PortfolioPage />} />
+            <Route path='council' element={<CouncilPage />} />
+            <Route path='' element={<TradePage />} />
+          </Route>
           <Route path='trade' element={<TradePage />} />
           <Route path='farm' element={<FarmPage />} />
           <Route path='lend' element={<LendPage />} />
@@ -27,13 +43,6 @@ export default function Routes() {
           <Route path='council' element={<CouncilPage />} />
           <Route path='' element={<TradePage />} />
         </Route>
-        <Route path='trade' element={<TradePage />} />
-        <Route path='farm' element={<FarmPage />} />
-        <Route path='lend' element={<LendPage />} />
-        <Route path='borrow' element={<BorrowPage />} />
-        <Route path='portfolio' element={<PortfolioPage />} />
-        <Route path='council' element={<CouncilPage />} />
-        <Route path='' element={<TradePage />} />
       </Route>
     </RoutesWrapper>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { WalletConnectProvider } from 'components/Wallet/WalletConnectProvider'
 import Routes from 'components/Routes'
-import Layout from 'pages/_layout'
 
 export default function Router() {
   return (
     <WalletConnectProvider>
       <BrowserRouter>
-        <Layout>
-          <Routes />
-        </Layout>
+        <Routes />
       </BrowserRouter>
     </WalletConnectProvider>
   )
