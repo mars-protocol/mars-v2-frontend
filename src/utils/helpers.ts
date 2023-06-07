35932dd23be4b28c599b3926fe0f10c9f985c180
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-06-07 11:28:21 +0200

    add calculation for max borrowings

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,9 @@ import BigNumber from 'bignumber.js'
 export function BN(n: BigNumber.Value) {
   return new BigNumber(n)
 }
+
+export function getApproximateHourlyInterest(amount: string, borrowRate: number) {
+  return BigNumber(borrowRate)
+    .div(24 * 365)
+    .times(amount)
+}
--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,4 +1,7 @@
+import BigNumber from 'bignumber.js'
+
 import { getBaseAsset } from 'utils/assets'
+import { BN } from 'utils/helpers'
 
 export const getTokenSymbol = (denom: string, marketAssets: Asset[]) =>
   marketAssets.find((asset) => asset.denom.toLowerCase() === denom.toLowerCase())?.symbol || ''
@@ -11,3 +14,8 @@ export const getTokenIcon = (denom: string, marketAssets: Asset[]) =>
 
 export const getTokenInfo = (denom: string, marketAssets: Asset[]) =>
   marketAssets.find((asset) => asset.denom.toLowerCase() === denom.toLowerCase()) || getBaseAsset()
+
+export function getTokenValue(coin: Coin, prices: Coin[]): BigNumber {
+  const price = prices.find((price) => price.denom === coin.denom)?.amount || '0'
+  return BN(price).times(coin.amount)
+}
