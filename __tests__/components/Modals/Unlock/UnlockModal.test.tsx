d2d8f060321a8b5e59c1b3681a0a2d0fa753453d
Linkie Link <linkielink.dev@gmail.com>
2023-07-06 23:24:06 +0200

    fix: adjust test

--- __tests__/components/Modals/Unlock/UnlockModal.test.tsx
+++ __tests__/components/Modals/Unlock/UnlockModal.test.tsx
@@ -41,26 +41,6 @@ describe('<UnlockModal />', () => {
   })
   it('should render', () => {
     const { container } = render(<UnlockModal />)
-    expect(mockedModal).toHaveBeenCalledTimes(1)
     expect(container).toBeInTheDocument()
-  })
-
-  describe('should set open attribute correctly', () => {
-    it('should set open = false when no modal is present in store', () => {
-      render(<UnlockModal />)
-      expect(mockedModal).toHaveBeenCalledWith(
-        expect.objectContaining({ open: false }),
-        expect.anything(),
-      )
-    })
-
-    it('should set open = true when no modal is present in store', () => {
-      useStore.setState({ unlockModal: { vault: mockedDepositedVault } })
-      render(<UnlockModal />)
-      expect(mockedModal).toHaveBeenLastCalledWith(
-        expect.objectContaining({ open: true }),
-        expect.anything(),
-      )
-    })
   })
 })
