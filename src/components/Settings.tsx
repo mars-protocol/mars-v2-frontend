c80904b05e08e485380e09a9a52693ce1c732d14
Linkie Link <linkielink.dev@gmail.com>
2023-07-06 23:15:35 +0200

    remove: reduceMotion settings check, as it got disabled with useLocalStorage

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,8 @@
 import Button from 'components/Button'
 import { Gear } from 'components/Icons'
-import { useAnimations } from 'hooks/useAnimations'
 import useStore from 'store'
 
 export default function Settings() {
-  useAnimations()
-
   return (
     <Button
       variant='solid'
