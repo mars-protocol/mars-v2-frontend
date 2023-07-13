b61c800b4da0d14bda189b833754d23a2bef7ff9
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-07-13 08:54:53 +0200

    remove usecallback

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 
 import OrderBook from 'components/Trade/OrderBook'
 import TradeChart from 'components/Trade/TradeChart'
@@ -10,20 +10,13 @@ export default function TradePage() {
   const [buyAsset, setBuyAsset] = useState(enabledMarketAssets[0])
   const [sellAsset, setSellAsset] = useState(enabledMarketAssets[1])
 
-  const handleChangeBuyAsset = useCallback((asset: Asset) => {
-    setBuyAsset(asset)
-  }, [])
-  const handleChangeSellAsset = useCallback((asset: Asset) => {
-    setSellAsset(asset)
-  }, [])
-
   return (
     <div className='grid h-full w-full grid-cols-[346px_auto] gap-4'>
       <TradeModule
         buyAsset={buyAsset}
         sellAsset={sellAsset}
-        onChangeBuyAsset={handleChangeBuyAsset}
-        onChangeSellAsset={handleChangeSellAsset}
+        onChangeBuyAsset={setBuyAsset}
+        onChangeSellAsset={setSellAsset}
       />
       <TradeChart buyAsset={buyAsset} sellAsset={sellAsset} />
       <OrderBook />
