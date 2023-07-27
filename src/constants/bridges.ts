47bd0d972817e1a1eaee5436973b2cdd8f443d26
Linkie Link <linkielink.dev@gmail.com>
2023-07-27 09:47:23 +0200

    feat: added TFM bridge

--- src/constants/bridges.ts
+++ src/constants/bridges.ts
@@ -1,5 +1,10 @@
 export const BRIDGES: Bridge[] = [
   {
+    name: 'TFM Bridge',
+    url: 'https://tfm.com/bridge?chainTo=osmosis-1',
+    image: '/images/bridges/tfm.png',
+  },
+  {
     name: 'Gravity bridge',
     url: 'https://bridge.blockscape.network',
     image: '/images/bridges/gravity.png',
