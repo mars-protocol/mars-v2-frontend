a86a2d32fc3b5a7a9fd2d92590122b303cd4cb17
Yusuf Seyrek <yusuf@delphilabs.io>
2023-08-16 16:00:10 +0300

    fix(swap response): parsing token out

--- src/utils/getTokenOutFromSwapResponse.ts
+++ src/utils/getTokenOutFromSwapResponse.ts
@@ -8,7 +8,8 @@ export default function getTokenOutFromSwapResponse(
       const events = rawLogs[0].events
       const tokenSwappedEvent = events.find((e: { type: string }) => e.type === 'token_swapped')
       const tokensOutValue = tokenSwappedEvent.attributes.find(
-        (a: { key: string }) => a.key === 'tokens_out',
+        (a: { key: string; value: string }) =>
+          a.key === 'tokens_out' && a.value.toLowerCase().includes(denom.toLowerCase()),
       ).value
       const amount = tokensOutValue.split(denom)[0]
 
