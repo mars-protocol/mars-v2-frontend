678de011bdb057fcd822906372d9d295af46d7da
Linkie Link <linkielink.dev@gmail.com>
2023-08-28 21:19:08 +0200

    tidy: value sanity

--- src/components/Account/HealthBar.tsx
+++ src/components/Account/HealthBar.tsx
@@ -14,9 +14,9 @@ interface Props {
 
 function calculateHealth(health: number) {
   const firstBarEnd = 43
-  const secondBarStart = 47
+  const secondBarStart = 46
   const secondBarEnd = 93
-  const thirdBarStart = 95
+  const thirdBarStart = 96
   const thirdBarEnd = 184
 
   if (health <= 10) return (firstBarEnd / 10) * health
@@ -36,7 +36,7 @@ export default function HealthBar(props: Props) {
         <svg version='1.1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 184 4'>
           <mask id='healthBarMask'>
             <path fill='#FFFFFF' d='M0,2c0-1.1,0.9-2,2-2h41.6v4H2C0.9,4,0,3.1,0,2z' />
-            <rect x='46' fill='#FFFFFF' width='47.2' height='4' />
+            <rect x='46' fill='#FFFFFF' width='47' height='4' />
             <path fill='#FFFFFF' d='M95.5,0H182c1.1,0,2,0.9,2,2s-0.9,2-2,2H95.5V0z' />
           </mask>
           <rect className='fill-white/10' width='184' height='4' mask='url(#healthBarMask)' />
