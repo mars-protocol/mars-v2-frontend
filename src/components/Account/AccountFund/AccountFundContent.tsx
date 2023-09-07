45b09006342f8faeef6249a61ed321ec0e2f004e
Linkie Link <linkielink.dev@gmail.com>
2023-09-07 10:32:26 +0200

    fix: updated AccountFundContent

--- src/components/Account/AccountFund/AccountFundContent.tsx
+++ src/components/Account/AccountFund/AccountFundContent.tsx
@@ -89,6 +89,7 @@ export default function AccountFundContent(props: Props) {
         focusComponent: null,
       })
   }, [props.accountId, deposit, fundingAssets, isLending])
+
   useEffect(() => {
     if (BN(baseBalance).isLessThan(defaultFee.amount[0].amount)) {
       useStore.setState({ focusComponent: { component: <WalletBridges /> } })
