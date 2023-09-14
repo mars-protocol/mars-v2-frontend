157e52ad25a0f38aec59760ed62939f0ca8ee172
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-09-14 11:59:22 +0200

    🐛add pagination for asset params

--- src/api/params/getAssetParams.ts
+++ src/api/params/getAssetParams.ts
@@ -1,11 +1,11 @@
 import { getParamsQueryClient } from 'api/cosmwasm-client'
 import { AssetParamsBaseForAddr } from 'types/generated/mars-params/MarsParams.types'
+import iterateContractQuery from 'utils/iterateContractQuery'
 
 export default async function getAssetParams(): Promise<AssetParamsBaseForAddr[]> {
   try {
     const paramsQueryClient = await getParamsQueryClient()
-
-    return paramsQueryClient.allAssetParams({})
+    return iterateContractQuery(paramsQueryClient.allAssetParams)
   } catch (ex) {
     throw ex
   }
