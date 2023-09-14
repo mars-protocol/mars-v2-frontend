b717909ee0d57415891e220e3270a0cafdba8828
Linkie Link <linkielink.dev@gmail.com>
2023-09-14 17:16:15 +0200

    fix: added empty wallet to prevent infinite pause

--- src/components/Account/AccountOverview.tsx
+++ src/components/Account/AccountOverview.tsx
@@ -12,6 +12,7 @@ import Loading from 'components/Loading'
 import Text from 'components/Text'
 import WalletBridges from 'components/Wallet/WalletBridges'
 import WalletConnectButton from 'components/Wallet/WalletConnectButton'
+import { EMPTY_WALLET } from 'constants/wallets'
 import useAccounts from 'hooks/useAccounts'
 import useBorrowMarketAssetsTableData from 'hooks/useBorrowMarketAssetsTableData'
 import useCurrentWalletBalance from 'hooks/useCurrentWalletBalance'
@@ -37,7 +38,7 @@ function ConnectInfo() {
 
 function Content() {
   const { address } = useParams()
-  const { data: accounts } = useAccounts(address ?? '')
+  const { data: accounts } = useAccounts(address ?? EMPTY_WALLET)
   const walletAddress = useStore((s) => s.address)
   const baseCurrency = useStore((s) => s.baseCurrency)
   const { availableAssets: borrowAvailableAssets, accountBorrowedAssets } =
--- src/components/Portfolio/PortfolioIntro.tsx
+++ src/components/Portfolio/PortfolioIntro.tsx
@@ -7,21 +7,19 @@ export default function PortfolioIntro() {
   const { address } = useParams()
   const walletAddress = useStore((s) => s.address)
 
-  if (!address && !walletAddress) return null
-
   return (
     <Intro
       text={
-        walletAddress ? (
-          <>
-            This is your <span className='text-white'>Portfolio</span>. Use it to get an overview
-            about all your Credit Accounts and their balances.
-          </>
-        ) : (
+        address && !walletAddress ? (
           <>
             This is the <span className='text-white'>Portfolio</span> of the address{' '}
             <span className='text-white'>{address}</span>. You can see all Credit Accounts of this
             address, but you can&apos;t interact with them.
+          </>
+        ) : (
+          <>
+            This is your <span className='text-white'>Portfolio</span>. Use it to get an overview
+            about all your Credit Accounts and their balances.
           </>
         )
       }
