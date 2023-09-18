c7c0e230c8fa7fbad45fa9a7461a3effbf50839d
Linkie Link <linkielink.dev@gmail.com>
2023-09-18 14:25:11 +0200

    MP-3413: added transition on update and success checkmark

--- src/components/Toaster/index.tsx
+++ src/components/Toaster/index.tsx
@@ -1,10 +1,11 @@
 import classNames from 'classnames'
 import { ReactNode } from 'react'
-import { toast as createToast, Slide, ToastContainer } from 'react-toastify'
+import { Slide, ToastContainer, toast as toastify } from 'react-toastify'
 import { mutate } from 'swr'
 
+import { CheckMark } from 'components/CheckMark'
 import { CircularProgress } from 'components/CircularProgress'
-import { CheckCircled, Cross, CrossCircled, ExternalLink } from 'components/Icons'
+import { Cross, CrossCircled, ExternalLink, Search } from 'components/Icons'
 import Text from 'components/Text'
 import { TextLink } from 'components/TextLink'
 import { DEFAULT_SETTINGS } from 'constants/defaultSettings'
@@ -17,11 +18,13 @@ import { formatAmountWithSymbol } from 'utils/formatters'
 import { BN } from 'utils/helpers'
 
 const toastBodyClasses = classNames(
-  'relative isolate m-0 flex w-full flex-wrap rounded-sm p-4 shadow-overlay backdrop-blur-lg',
+  'flex flex-wrap w-full group/transaction',
+  'rounded-sm p-4 shadow-overlay backdrop-blur-lg',
   'before:content-[" "] before:absolute before:inset-0 before:-z-1 before:rounded-sm before:p-[1px] before:border-glas',
 )
 
-function isPromise(object: any): object is ToastPending {
+function isPromise(object?: any): object is ToastPending {
+  if (!object) return false
   return 'promise' in object
 }
 
@@ -55,9 +58,9 @@ export default function Toaster() {
 
   const handlePending = (toast: ToastPending) => {
     const Content = () => (
-      <div className={classNames(toastBodyClasses, 'bg-info-bg/20')}>
+      <div className='relative flex flex-wrap w-full m-0 isolate'>
         <div className='flex items-center w-full gap-2 mb-2'>
-          <div className='rounded-sm p-1.5 pt-1 bg-info w-7 h-7'>
+          <div className='rounded-sm p-1.5 pt-1 bg-info w-7 h-7 flex items-center'>
             <CircularProgress size={16} />
           </div>
           <Text className='flex items-center font-bold text-info'>Pending Transaction</Text>
@@ -70,8 +73,9 @@ export default function Toaster() {
       </div>
     )
 
-    createToast(Content, {
+    toastify(Content, {
       toastId: toast.id,
+      className: classNames(toastBodyClasses, 'toast-pending'),
       icon: false,
       draggable: false,
       closeOnClick: false,
@@ -80,19 +84,25 @@ export default function Toaster() {
     })
   }
 
-  const handleResponse = (toast: ToastResponse) => {
+  const handleResponse = (toast: ToastResponse, details?: boolean) => {
     const isError = toast?.isError
     if (!isError) addTransaction(toast)
+    const generalMessage = isError ? 'Transaction failed!' : 'Transaction completed successfully!'
+    const showDetailElement = !!(!details && toast.hash)
     const Msg = () => (
-      <div
-        className={classNames(toastBodyClasses, isError ? 'bg-error-bg/20' : 'bg-success-bg/20')}
-      >
-        <div className='flex w-full gap-2 mb-4'>
-          <div className={classNames('rounded-sm p-1.5', isError ? 'bg-error' : 'bg-success')}>
-            <span className='block w-4 h-4 text-white'>
-              {isError ? <CrossCircled /> : <CheckCircled />}
-            </span>
-          </div>
+      <div className='relative flex flex-wrap w-full m-0 isolate'>
+        <div className='flex w-full gap-2 mb-2'>
+          {isError ? (
+            <div className='rounded-sm p-1.5 bg-error'>
+              <span className='block w-4 h-4 text-white'>
+                <CrossCircled />
+              </span>
+            </div>
+          ) : (
+            <div className='rounded-sm p-1.5 pt-1 bg-success w-7 h-7  flex items-center'>
+              <CheckMark size={16} />
+            </div>
+          )}
           <Text
             className={classNames(
               'flex items-center font-bold',
@@ -102,46 +112,82 @@ export default function Toaster() {
             {isError ? (toast.title ? toast.title : 'Error') : 'Success'}
           </Text>
         </div>
-        {!isError && toast.accountId && (
-          <Text className='mb-1 font-bold text-white'>{`Credit Account ${toast.accountId}`}</Text>
-        )}
-        {toast.message && (
-          <Text size='sm' className='w-full mb-1 text-white'>
-            {toast.message}
+        <Text size='sm' className='w-full mb-1 text-white'>
+          {showDetailElement ? generalMessage : toast.message}
+        </Text>
+        {showDetailElement && (
+          <Text
+            size='sm'
+            className='flex items-center w-auto pb-0.5 text-white border-b border-white border-dashed group-hover/transaction:opacity-0'
+          >
+            <Search className='w-3 h-3 mr-1' />
+            Transaction Details
           </Text>
         )}
-        {!isError && toast.content?.length > 0 && generateToastContent(toast.content)}
-        {toast.hash && (
-          <div className='w-full'>
-            <TextLink
-              href={`${EXPLORER_TX_URL}${toast.hash}`}
-              target='_blank'
-              className={classNames(
-                'leading-4 underline mt-4 hover:no-underline hover:text-white',
-                isError ? 'text-error' : 'text-success',
-              )}
-              title={`View on ${EXPLORER_NAME}`}
-            >
-              {`View on ${EXPLORER_NAME}`}
-              <ExternalLink className='-mt-0.5 ml-2 inline w-3.5' />
-            </TextLink>
-          </div>
-        )}
-        <div className='absolute right-4 top-4 '>
+        <div
+          className={classNames(
+            'w-full flex-wrap',
+            showDetailElement && 'hidden group-hover/transaction:flex',
+          )}
+        >
+          {!isError && toast.accountId && (
+            <Text className='mb-1 font-bold text-white'>{`Credit Account ${toast.accountId}`}</Text>
+          )}
+          {showDetailElement && toast.message && (
+            <Text size='sm' className='w-full mb-1 text-white'>
+              {toast.message}
+            </Text>
+          )}
+          {!isError && toast.content?.length > 0 && generateToastContent(toast.content)}
+          {toast.hash && (
+            <div className='w-full'>
+              <TextLink
+                href={`${EXPLORER_TX_URL}${toast.hash}`}
+                target='_blank'
+                className={classNames(
+                  'leading-4 underline mt-4 hover:no-underline hover:text-white',
+                  isError ? 'text-error' : 'text-success',
+                )}
+                title={`View on ${EXPLORER_NAME}`}
+              >
+                {`View on ${EXPLORER_NAME}`}
+                <ExternalLink className='-mt-0.5 ml-2 inline w-3.5' />
+              </TextLink>
+            </div>
+          )}
+        </div>
+        <div className='absolute top-0 right-0'>
           <Cross className={classNames('h-2 w-2', isError ? 'text-error' : 'text-success')} />
         </div>
       </div>
     )
 
-    createToast.update(toast.id, {
-      render: Msg,
-      icon: false,
-      draggable: false,
-      closeOnClick: true,
-      autoClose: 5000,
-      progressClassName: classNames('h-[1px] bg-none', isError ? 'bg-error' : 'bg-success'),
-      hideProgressBar: false,
-    })
+    const toastElement = document.getElementById(String(toast.id))
+
+    if (toastElement) {
+      toastify.update(toast.id, {
+        render: Msg,
+        className: toastBodyClasses,
+        type: isError ? 'error' : 'success',
+        icon: false,
+        draggable: false,
+        closeOnClick: true,
+        autoClose: 500000,
+        progressClassName: classNames('h-[1px] bg-none', isError ? 'bg-error' : 'bg-success'),
+        hideProgressBar: false,
+      })
+    } else {
+      toastify(Msg, {
+        toastId: toast.id,
+        className: toastBodyClasses,
+        type: isError ? 'error' : 'success',
+        icon: false,
+        draggable: false,
+        closeOnClick: true,
+        autoClose: 500000,
+        progressClassName: classNames('h-[1px] bg-none', isError ? 'bg-error' : 'bg-success'),
+      })
+    }
 
     useStore.setState({ toast: null })
     mutate(() => true)
@@ -162,9 +208,8 @@ export default function Toaster() {
       newestOnTop
       closeOnClick={false}
       transition={reduceMotion ? undefined : Slide}
-      className='p-0'
-      toastClassName='top-[73px] z-20 m-0 mb-4 flex w-full bg-transparent p-0'
-      bodyClassName='p-0 m-0 w-full flex -z-1'
+      bodyClassName='p-0 m-0 -z-1'
+      className='mt-[73px]'
     />
   )
 }
