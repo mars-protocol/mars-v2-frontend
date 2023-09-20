5bf46d4da07c9d2de91ebc7432fe206a9927313b
Linkie Link <linkielink.dev@gmail.com>
2023-09-20 15:53:33 +0200

    fix: all your codebase belongs to us (#492)

--- src/components/Trade/TradeModule/SwapForm/TradeSummary.tsx
+++ src/components/Trade/TradeModule/SwapForm/TradeSummary.tsx
@@ -49,18 +49,25 @@ export default function TradeSummary(props: Props) {
     [buyAsset.symbol, routes],
   )
 
+  const infoLineClasses = 'flex flex-row justify-between flex-1 mb-1 text-xs text-white'
+
   return (
-    <div className={classNames(containerClassName, className.container)}>
-      <div className={className.summaryWrapper}>
-        <span className={className.title}>Summary</span>
-        <div className={className.infoLine}>
-          <span className={className.infoLineLabel}>Fees</span>
+    <div
+      className={classNames(
+        containerClassName,
+        'flex flex-1 flex-col bg-white bg-opacity-5 rounded border-[1px] border-white border-opacity-20',
+      )}
+    >
+      <div className='flex flex-col flex-1 m-3'>
+        <span className='mb-2 text-xs font-bold'>Summary</span>
+        <div className={infoLineClasses}>
+          <span className='opacity-40'>Fees</span>
           <span>{formatAmountWithSymbol(estimatedFee.amount[0])}</span>
         </div>
         {isMargin && (
           <>
-            <div className={className.infoLine}>
-              <span className={className.infoLineLabel}>Borrowing</span>
+            <div className={infoLineClasses}>
+              <span className='opacity-40'>Borrowing</span>
               <FormattedNumber
                 amount={borrowAmount.toNumber()}
                 options={{
@@ -74,15 +81,15 @@ export default function TradeSummary(props: Props) {
                 animate
               />
             </div>
-            <div className={className.infoLine}>
-              <span className={className.infoLineLabel}>Borrow rate</span>
+            <div className={infoLineClasses}>
+              <span className='opacity-40'>Borrow rate</span>
               <span>{formatPercent(borrowRate || 0)}</span>
             </div>
           </>
         )}
 
-        <div className={className.infoLine}>
-          <span className={className.infoLineLabel}>Route</span>
+        <div className={infoLineClasses}>
+          <span className='opacity-40'>Route</span>
           <span>{parsedRoutes}</span>
         </div>
       </div>
@@ -97,13 +104,4 @@ export default function TradeSummary(props: Props) {
       />
     </div>
   )
-}
-
-const className = {
-  container:
-    'flex flex-1 flex-col bg-white bg-opacity-5 rounded border-[1px] border-white border-opacity-20 ',
-  title: 'text-xs font-bold mb-2',
-  summaryWrapper: 'flex flex-1 flex-col m-3',
-  infoLine: 'flex flex-1 flex-row text-xs text-white justify-between mb-1',
-  infoLineLabel: 'opacity-40',
 }
