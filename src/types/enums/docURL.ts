612b67f85180af41bf697e4cb1c93629520a2a06
Linkie Link <linkielink.dev@gmail.com>
2023-10-10 09:20:37 +0200

    fix: updated doc urls according to Dino

--- src/types/enums/docURL.ts
+++ src/types/enums/docURL.ts
@@ -1,13 +1,12 @@
 export enum DocURL {
   ADVANCED_TRADING_URL = 'https://docs.marsprotocol.io/docs/learn/mars-v2/trade/margin',
   BORROW_LENDING_URL = 'https://docs.marsprotocol.io/docs/learn/mars-v2/borrow',
-  COOKIE_POLICY_URL = 'https://docs.marsprotocol.io/mars-protocol/terms-of-service/mars-cookie-policy',
-  CONCENTRATED_LIQUIDITY_URL = 'https://docs.marsprotocol.io',
+  COOKIE_POLICY_URL = 'https://docs.marsprotocol.io/docs/overview/legal/cookie-policy',
   DOCS_URL = 'https://docs.marsprotocol.io/',
   MANAGE_ACCOUNT_URL = 'https://docs.marsprotocol.io/docs/learn/tutorials/credit-accounts/credit-accounts-intro',
   ROVER_INTRO_URL = 'https://docs.marsprotocol.io/docs/learn/mars-v2/credit-accounts',
-  PRIVACY_POLICY_URL = 'https://docs.marsprotocol.io/mars-protocol/terms-of-service/mars-privacy-policy',
+  PRIVACY_POLICY_URL = 'https://docs.marsprotocol.io/docs/overview/legal/privacy-policy',
   TERMS_OF_SERVICE_URL = 'https://docs.marsprotocol.io/docs/overview/legal/terms-of-service',
-  TRADING_INTRO_URL = 'https://docs.marsprotocol.io/docs/learn/tutorials/trading/spot-trading',
+  TRADING_INTRO_URL = 'https://docs.marsprotocol.io/docs/learn/tutorials/trading/trading-intro',
   WALLET_INTRO_URL = 'https://docs.marsprotocol.io/docs/learn/tutorials/basics/connecting-your-wallet',
 }
