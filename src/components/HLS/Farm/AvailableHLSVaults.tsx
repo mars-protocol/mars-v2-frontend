8c7d05ed622462ca932780c992143c8d337028f0
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-10-17 12:10:05 +0200

    🧽 clean up PR

--- src/components/HLS/Farm/AvailableHLSVaults.tsx
+++ src/components/HLS/Farm/AvailableHLSVaults.tsx
@@ -1,7 +1,3 @@
-import Table from 'components/HLS/Farm/Table'
-import useHLSVaults from 'hooks/useHLSVaults'
-
 export default function AvailableHlsVaults() {
-  const { data: HLSStrategies } = useHLSVaults()
-  return <Table data={HLSStrategies} />
+  return null
 }
