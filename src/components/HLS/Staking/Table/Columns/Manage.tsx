5a775bcc6bc77355f0185869e999d4d6ea08f301
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-11-03 15:19:00 +0100

    ✨Added dropdown button (#614)

--- src/components/HLS/Staking/Table/Columns/Manage.tsx
+++ src/components/HLS/Staking/Table/Columns/Manage.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
 
-import Button from 'components/Button'
+import DropDownButton from 'components/Button/DropDownButton'
+import { ArrowDownLine, HandCoins, Plus } from 'components/Icons'
 
 export const MANAGE_META = { id: 'manage' }
+
+const ITEMS: DropDownItem[] = [
+  {
+    icon: <Plus width={16} />,
+    text: 'Deposit more',
+    onClick: () => {},
+  },
+  {
+    icon: <HandCoins />,
+    text: 'Repay',
+    onClick: () => {},
+  },
+  {
+    icon: <ArrowDownLine />,
+    text: 'Withdraw',
+    onClick: () => {},
+  },
+]
 
 interface Props {
   account: HLSAccountWithStrategy
 }
 
 export default function Manage(props: Props) {
-  // TODO: Impelement dropdown
-  return <Button text='Manage' color='tertiary' />
+  return <DropDownButton items={ITEMS} text='Manage' color='tertiary' />
 }
