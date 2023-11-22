e3d3d30700f2f1fd381678b04ab2368e212800ef
Linkie Link <linkielink.dev@gmail.com>
2023-11-22 17:01:25 +0100

    feat: added a first Chart component

--- src/components/Stats/StatsTrading.tsx
+++ src/components/Stats/StatsTrading.tsx
@@ -1,3 +1,36 @@
+import Chart from 'components/Chart'
+
 export default function StatsTrading() {
-  return <div className='flex-1'>Stats Trading</div>
+  const totalSwapVolume = [
+    {
+      date: '2023-11-15',
+      value: 2501271,
+    },
+    {
+      date: '2023-11-16',
+      value: 2804718,
+    },
+    {
+      date: '2023-11-17',
+      value: 4901520,
+    },
+    {
+      date: '2023-11-18',
+      value: 6500000,
+    },
+    {
+      date: '2023-11-19',
+      value: 7486720,
+    },
+    {
+      date: '2023-11-20',
+      value: 8412721,
+    },
+    {
+      date: '2023-11-21',
+      value: 10432321,
+    },
+  ]
+
+  return <Chart title='Total Swap Volume' data={totalSwapVolume} />
 }
