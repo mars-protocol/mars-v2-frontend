e68c0c5cb39e8be42379f9f621dd50a14aebb8f9
Linkie Link <linkielink.dev@gmail.com>
2023-11-26 11:33:46 +0100

    fix: fixed the withdraw from vaults modal

--- src/components/Modals/WithdrawFromVaultsModal.tsx
+++ src/components/Modals/WithdrawFromVaultsModal.tsx
@@ -7,18 +7,20 @@ import Modal from 'components/Modal'
 import Text from 'components/Text'
 import { DEFAULT_SETTINGS } from 'constants/defaultSettings'
 import { LocalStorageKeys } from 'constants/localStorageKeys'
+import { ORACLE_DENOM } from 'constants/oracle'
 import useAccountId from 'hooks/useAccountId'
 import useLocalStorage from 'hooks/useLocalStorage'
+import usePrices from 'hooks/usePrices'
 import useStore from 'store'
 import { BNCoin } from 'types/classes/BNCoin'
+import { byDenom } from 'utils/array'
 import { getAssetByDenom } from 'utils/assets'
-import { demagnify } from 'utils/formatters'
 
 export default function WithdrawFromVaultsModal() {
   const modal = useStore((s) => s.withdrawFromVaultsModal)
   const accountId = useAccountId()
+  const { data: prices } = usePrices()
   const withdrawFromVaults = useStore((s) => s.withdrawFromVaults)
-  const baseCurrency = useStore((s) => s.baseCurrency)
   const [slippage] = useLocalStorage<number>(LocalStorageKeys.SLIPPAGE, DEFAULT_SETTINGS.slippage)
 
   function onClose() {
@@ -37,6 +39,8 @@ export default function WithdrawFromVaultsModal() {
 
   if (!modal) return null
 
+  console.log(modal)
+
   return (
     <Modal
       onClose={onClose}
@@ -52,12 +56,18 @@ export default function WithdrawFromVaultsModal() {
       {modal ? (
         <div className='flex flex-wrap w-full gap-4'>
           {modal.map((vault) => {
-            const positionValue = vault.values.primary.plus(vault.values.secondary)
-            const coin = BNCoin.fromDenomAndBigNumber(baseCurrency.denom, positionValue)
+            const positionValue = vault.values.unlocking
+            const coin = BNCoin.fromDenomAndBigNumber(ORACLE_DENOM, positionValue)
             const primaryAsset = getAssetByDenom(vault.denoms.primary)
             const secondaryAsset = getAssetByDenom(vault.denoms.secondary)
 
             if (!primaryAsset || !secondaryAsset) return null
+            const primaryAssetPrice = prices.find(byDenom(primaryAsset.denom))?.amount ?? 1
+            const secondaryAssetPrice = prices.find(byDenom(secondaryAsset.denom))?.amount ?? 1
+
+            const primaryAssetAmount = positionValue.dividedBy(primaryAssetPrice).dividedBy(2)
+            const secondaryAssetAmount = positionValue.dividedBy(secondaryAssetPrice).dividedBy(2)
+
             return (
               <div className='flex items-center gap-4' key={vault.unlockId}>
                 <DoubleLogo
@@ -73,15 +83,21 @@ export default function WithdrawFromVaultsModal() {
                 <div className='flex flex-wrap'>
                   <DisplayCurrency coin={coin} className='w-full text-right' />
                   <FormattedNumber
-                    amount={demagnify(vault.amounts.primary, primaryAsset)}
+                    amount={Number(primaryAssetAmount.toPrecision(4))}
                     className='w-full text-sm text-right text-white/50'
-                    options={{ suffix: ` ${vault.symbols.primary}` }}
+                    options={{
+                      suffix: ` ${vault.symbols.primary}`,
+                      maxDecimals: primaryAsset.decimals,
+                    }}
                     animate
                   />
                   <FormattedNumber
-                    amount={demagnify(vault.amounts.secondary, secondaryAsset)}
+                    amount={Number(secondaryAssetAmount.toPrecision(4))}
                     className='w-full text-sm text-right text-white/50'
-                    options={{ suffix: ` ${vault.symbols.secondary}` }}
+                    options={{
+                      suffix: ` ${vault.symbols.secondary}`,
+                      maxDecimals: secondaryAsset.decimals,
+                    }}
                     animate
                   />
                 </div>
