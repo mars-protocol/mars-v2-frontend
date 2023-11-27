e34c4464f7a443410ab3718d5c52ab7788a7aa53
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-11-27 14:43:12 +0100

    adjust logic of auto-lend for switch component

--- src/components/Switch/SwitchAutoLend.tsx
+++ src/components/Switch/SwitchAutoLend.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames'
-import { useEffect } from 'react'
+import { useCallback } from 'react'
 
 import SwitchWithLabel from 'components/Switch/SwitchWithLabel'
+import { DEFAULT_SETTINGS } from 'constants/defaultSettings'
 import { LocalStorageKeys } from 'constants/localStorageKeys'
 import useAutoLend from 'hooks/useAutoLend'
 import useLocalStorage from 'hooks/useLocalStorage'
@@ -14,30 +15,38 @@ interface Props {
 export default function SwitchAutoLend(props: Props) {
   const { accountId, className } = props
   const { autoLendEnabledAccountIds, disableAutoLend, enableAutoLend } = useAutoLend()
-  const isAutoLendEnabled = autoLendEnabledAccountIds.includes(accountId)
-  const [lendAssets, setLendAssets] = useLocalStorage<boolean | null>(
+  const isAutoLendEnabledForAccount = autoLendEnabledAccountIds.includes(accountId)
+  const [isAutoLendEnabled, setIsAutoLendEnabled] = useLocalStorage<boolean>(
     LocalStorageKeys.LEND_ASSETS,
-    null,
+    DEFAULT_SETTINGS.lendAssets,
   )
 
-  useEffect(() => {
-    if (lendAssets && !isAutoLendEnabled) enableAutoLend(accountId)
-  }, [lendAssets, isAutoLendEnabled])
+  const handleToggle = useCallback(() => {
+    if (!isAutoLendEnabledForAccount) {
+      enableAutoLend(accountId)
+      return
+    }
+
+    if (isAutoLendEnabled) {
+      setIsAutoLendEnabled(false)
+      disableAutoLend(accountId)
+    }
+  }, [
+    accountId,
+    disableAutoLend,
+    enableAutoLend,
+    isAutoLendEnabled,
+    isAutoLendEnabledForAccount,
+    setIsAutoLendEnabled,
+  ])
 
   return (
     <div className={classNames('w-full', className)}>
       <SwitchWithLabel
         name={`isLending-${accountId}`}
         label='Lend assets to earn yield'
-        value={isAutoLendEnabled}
-        onChange={() => {
-          if (isAutoLendEnabled) {
-            setLendAssets(false)
-            disableAutoLend(accountId)
-            return
-          }
-          enableAutoLend(accountId)
-        }}
+        value={isAutoLendEnabledForAccount}
+        onChange={handleToggle}
         tooltip={`Fund your account and lend assets effortlessly! By lending, you'll earn attractive interest (APY) without impacting your loan to value (LTV). It's a win-win situation - don't miss out on this easy opportunity to grow your holdings!`}
       />
     </div>
