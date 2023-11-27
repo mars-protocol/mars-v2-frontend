b1a39c0dce6a62cb3a56c3bd2641c404d47dc071
Linkie Link <linkielink.dev@gmail.com>
2023-11-27 11:55:03 +0100

    refactor: moved the autolend logic to the switch

--- src/components/Switch/SwitchAutoLend.tsx
+++ src/components/Switch/SwitchAutoLend.tsx
@@ -1,33 +1,44 @@
 import classNames from 'classnames'
+import { useEffect } from 'react'
 
 import SwitchWithLabel from 'components/Switch/SwitchWithLabel'
+import { DEFAULT_SETTINGS } from 'constants/defaultSettings'
+import { LocalStorageKeys } from 'constants/localStorageKeys'
 import useAutoLend from 'hooks/useAutoLend'
+import useLocalStorage from 'hooks/useLocalStorage'
 
 interface Props {
   accountId: string
   className?: string
-  onChange?: () => void
-  value?: boolean
 }
 
 export default function SwitchAutoLend(props: Props) {
   const { accountId, className } = props
-  const { autoLendEnabledAccountIds, toggleAutoLend } = useAutoLend()
+  const { autoLendEnabledAccountIds, disableAutoLend, enableAutoLend } = useAutoLend()
   const isAutoLendEnabled = autoLendEnabledAccountIds.includes(accountId)
+  const [lendAssets, setLendAssets] = useLocalStorage<boolean>(
+    LocalStorageKeys.LEND_ASSETS,
+    DEFAULT_SETTINGS.lendAssets,
+  )
 
-  function handleToggle() {
-    if (props.onChange) return props.onChange()
-
-    toggleAutoLend(accountId)
-  }
+  useEffect(() => {
+    if (lendAssets && !isAutoLendEnabled) enableAutoLend(accountId)
+  }, [lendAssets, isAutoLendEnabled])
 
   return (
     <div className={classNames('w-full', className)}>
       <SwitchWithLabel
-        name='isLending'
+        name={`isLending-${accountId}`}
         label='Lend assets to earn yield'
-        value={props.value !== undefined ? props.value : isAutoLendEnabled}
-        onChange={handleToggle}
+        value={isAutoLendEnabled}
+        onChange={() => {
+          if (isAutoLendEnabled) {
+            setLendAssets(false)
+            disableAutoLend(accountId)
+            return
+          }
+          enableAutoLend(accountId)
+        }}
         tooltip={`Fund your account and lend assets effortlessly! By lending, you'll earn attractive interest (APY) without impacting your loan to value (LTV). It's a win-win situation - don't miss out on this easy opportunity to grow your holdings!`}
       />
     </div>
