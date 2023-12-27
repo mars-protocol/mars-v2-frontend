2b1414192af23f32fe4420deb39d1bbaf4e809dd
Linkie Link <linkielink.dev@gmail.com>
2023-12-27 21:47:26 +0100

    feat: added PYTH logo to the trade chart

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -79,7 +79,7 @@ export default function DesktopHeader() {
           </div>
         ) : (
           <div className='flex gap-4'>
-            {isOracleStale && <OracleResyncButton />}
+            {address && isOracleStale && <OracleResyncButton />}
             {accountId && <RewardsCenter />}
             {address && !isHLS && <AccountMenu />}
             <Wallet />
