c31cce268286f8a3a475a6efb95e3289a3203393
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2024-01-11 14:51:54 +0100

    fix swap fee for osmosis

--- src/api/swap/getOsmosisSwapFee.ts
+++ src/api/swap/getOsmosisSwapFee.ts
@@ -1,14 +1,23 @@
-export default async function getPools(
+import { BN_ZERO } from 'constants/math'
+import { STANDARD_SWAP_FEE } from 'utils/constants'
+
+export default async function getOsmosisSwapFee(
   chainConfig: ChainConfig,
   poolIds: string[],
-): Promise<Pool[]> {
+): Promise<number> {
   const promises = poolIds.map((poolId) =>
     fetch(chainConfig.endpoints.pools.replace('POOL_ID', poolId)),
   )
 
   const responses = await Promise.all(promises)
 
-  return await Promise.all(responses.map(async (pool) => (await pool.json()).pool as Pool))
+  const pools = await Promise.all(responses.map(async (pool) => (await pool.json()).pool as Pool))
+
+  if (!pools?.length) return STANDARD_SWAP_FEE
+
+  return pools
+    .reduce((acc, pool) => acc.plus(pool?.pool_params?.swap_fee || STANDARD_SWAP_FEE), BN_ZERO)
+    .toNumber()
 }
 
 interface Pool {
