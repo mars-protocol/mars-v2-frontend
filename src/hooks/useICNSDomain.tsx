24345f1fe0b074d70b9cf8d369c778e0af5196e2
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2024-01-30 10:44:58 +0100

    moved api/icns and api/balances to hooks

--- src/hooks/useICNSDomain.tsx
+++ src/hooks/useICNSDomain.tsx
@@ -1,12 +1,20 @@
 import useSWR from 'swr'
 
-import getICNS from 'api/wallets/getICNS'
 import useChainConfig from 'hooks/useChainConfig'
+import { ChainInfoID } from 'types/enums/wallet'
+
+import useClients from './useClients'
 
 export default function useICNSDomain(address?: string) {
   const chainConfig = useChainConfig()
+  const clients = useClients()
+  const enabled = !!clients && chainConfig.id === ChainInfoID.Osmosis1 && address
 
-  return useSWR(`ICNS-${address}`, () => getICNS(chainConfig, address), {
-    revalidateOnFocus: false,
-  })
+  return useSWR(
+    enabled && `chains/${chainConfig.id}/${address}/icns`,
+    () => clients!.icns.primaryName({ address: address! }),
+    {
+      revalidateOnFocus: false,
+    },
+  )
 }
