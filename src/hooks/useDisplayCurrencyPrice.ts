784f7fc71247129cb0026adc527320f896763db4
Linkie Link <linkielink.dev@gmail.com>
2024-01-30 14:23:07 +0100

    fix: unfix

--- src/hooks/useDisplayCurrencyPrice.ts
+++ src/hooks/useDisplayCurrencyPrice.ts
@@ -34,8 +34,6 @@ function useDisplayCurrencyPrice() {
 
   const convertAmount = useCallback(
     (asset: Asset, amount: string | number | BigNumber) => {
-      if (!asset) return BN_ZERO
-
       return (
         getConversionRate(asset.denom)?.multipliedBy(BN(amount).shiftedBy(-asset.decimals)) ??
         BN_ZERO
