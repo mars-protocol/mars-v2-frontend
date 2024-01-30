77728d14508317e6d7a3e7530f5fd13d6e002d36
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2024-01-30 14:47:10 +0100

    moved api/incentives to hooks

--- src/hooks/useUnclaimedRewards.tsx
+++ src/hooks/useUnclaimedRewards.tsx
@@ -1,21 +1,44 @@
 import useSWR from 'swr'
 
-import getUnclaimedRewards from 'api/incentives/getUnclaimedRewards'
 import useAccountId from 'hooks/useAccountId'
 import useChainConfig from 'hooks/useChainConfig'
+import useClients from 'hooks/useClients'
 import { BNCoin } from 'types/classes/BNCoin'
+import iterateContractQuery from 'utils/iterateContractQuery'
 
 export default function useUserUnclaimedRewards() {
   const accountId = useAccountId()
   const chainConfig = useChainConfig()
+  const clients = useClients()
+
+  const enabled = !!accountId && !!clients
 
   return useSWR(
-    `chains/${chainConfig.id}/accounts/${accountId}/unclaimed-rewards`,
-    () => getUnclaimedRewards(chainConfig, accountId ?? ''),
+    enabled && `chains/${chainConfig.id}/accounts/${accountId}/unclaimed-rewards`,
+    () => getUnclaimedRewards(clients!, accountId!),
     {
       fallbackData: [] as BNCoin[],
       isPaused: () => !accountId,
       revalidateOnFocus: false,
     },
   )
+}
+
+async function getUnclaimedRewards(clients: ContractClients, accountId: string): Promise<BNCoin[]> {
+  try {
+    const unclaimedRewards = await iterateContractQuery(() =>
+      clients.incentives.userUnclaimedRewards({
+        user: clients.creditManager.contractAddress,
+        accountId,
+      }),
+    )
+
+    if (unclaimedRewards.length === 0) return []
+
+    return await Promise.all(
+      unclaimedRewards.map((reward) => new BNCoin({ denom: reward.denom, amount: reward.amount })),
+    )
+  } catch (ex) {
+    return []
+  }
 }
