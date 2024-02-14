ea07d5f11e2147de01210d46eda4d0baac8499f5
Linkie Link <linkielink.dev@gmail.com>
2024-02-14 11:47:42 +0100

    tidy: cleanup

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,9 +1,7 @@
 export const getUrl = (baseUrl: string, path: string): string => {
   const isPlaceholder = baseUrl.split('APP_').length > 1
 
-  if (isPlaceholder) {
-    return baseUrl + '/' + path
-  }
+  if (isPlaceholder) return baseUrl + '/' + path
 
   const url = new URL(baseUrl)
 
