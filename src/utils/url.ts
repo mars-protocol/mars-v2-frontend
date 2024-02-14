26f1ef4a2c03e68582006fa27034dfbfa1bb0654
Linkie Link <linkielink.dev@gmail.com>
2024-02-14 11:58:48 +0100

    Fix docker build (#805)
    * fix: fixed the docker build
    
    * tidy: cleanup

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,8 @@
 export const getUrl = (baseUrl: string, path: string): string => {
+  const isPlaceholder = baseUrl.split('APP_').length > 1
+
+  if (isPlaceholder) return baseUrl + '/' + path
+
   const url = new URL(baseUrl)
 
   if (process.env.NEXT_PUBLIC_API_KEY)
