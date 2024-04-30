3af43620c5b0967d45662e73c146175e5576b040
Linkie Link <linkielink.dev@gmail.com>
2024-04-30 12:49:09 +0200

    feat: working on the validation

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import Text from 'components/common/Text'
 import React, { useEffect, useState } from 'react'
 
 interface Props {
@@ -11,6 +12,8 @@ interface Props {
   onChange: (value: string) => void
   onBlur?: () => void
   onFocus?: () => void
+  error?: boolean
+  errorMessage?: string
 }
 
 export default function TextInput(props: Props) {
@@ -27,9 +30,19 @@ export default function TextInput(props: Props) {
   }
 
   return (
-    <div className={classNames('relative', props.className)}>
+    <div
+      className={classNames(
+        'relative',
+        props.errorMessage && props.error && 'mb-4',
+        props.className,
+      )}
+    >
       {props.label && (
-        <div className='absolute flex items-center h-full pl-2 border-r w-18 border-white/10 text-white/50'>
+        <div
+          className={classNames(
+            'absolute flex items-center h-full pl-2 border-r w-18 text-white/50 border-white/10',
+          )}
+        >
           {props.label}
         </div>
       )}
@@ -41,13 +54,20 @@ export default function TextInput(props: Props) {
         onBlur={props.onBlur}
         disabled={props.disabled}
         className={classNames(
-          'w-full hover:cursor-pointer appearance-none p-4 py-2 outline-none bg-white/5 border rounded-sm border-white/10 ',
-          'focus:border-white/20 focus:bg-white/10',
+          'w-full hover:cursor-pointer appearance-none p-4 py-2 outline-none border rounded-sm',
           props.label && 'pl-20',
           props.disabled && 'pointer-events-none',
+          props.error
+            ? ' bg-loss/5 border-loss/10 focus:border-loss/20 focus:bg-loss/10'
+            : 'bg-white/5 border-white/10 focus:border-white/20 focus:bg-white/10',
         )}
         placeholder={props.placeholder ?? '0'}
       />
+      {props.error && props.errorMessage && (
+        <Text size='xs' className='absolute left-0 -bottom-5 text-loss'>
+          {props.errorMessage}
+        </Text>
+      )}
     </div>
   )
 }
