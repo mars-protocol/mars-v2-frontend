7966ecae4f0b50c78ba212c0c9130a793e664c8c
Linkie Link <linkielink.dev@gmail.com>
2024-06-03 10:03:52 +0200

    fix: remove input searchParams

--- src/api/assets/getAstroportAssets.ts
+++ src/api/assets/getAstroportAssets.ts
@@ -1,9 +1,7 @@
 import { convertAstroportAssetsResponse } from 'utils/assets'
 
 export default async function getAssets(chainConfig: ChainConfig) {
-  const searchParams = { json: { chainId: chainConfig.id } }
   const uri = new URL(chainConfig.endpoints.dexAssets)
-  uri.searchParams.append('input', JSON.stringify(searchParams))
   try {
     const assets = await fetch(uri.toString()).then(async (res) => {
       const data = (await res.json()) as AstroportAsset[]
