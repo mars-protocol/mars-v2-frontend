e4aeda91133bdfcd7b4e50844700ab81cea3eabe
Linkie Link <linkielink.dev@gmail.com>
2024-07-03 12:21:12 +0200

    tidy: remove console

--- src/hooks/assets/useDepositEnabledAssets.ts
+++ src/hooks/assets/useDepositEnabledAssets.ts
@@ -4,8 +4,6 @@ import { useMemo } from 'react'
 export default function useDepositEnabledAssets() {
   const { data: assets } = useAssets()
 
-  console.log('assets', assets)
-
   return useMemo(
     () => assets.filter((asset) => asset.isDepositEnabled && !asset.isPoolToken),
     [assets],
