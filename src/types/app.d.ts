115cc0eaaef203533abd7308c8fafc43d5a027a0
Monkmansteve <sxatzakis@yahoo.gr>
2024-08-11 22:47:57 +0300

    chore: remove old modal from modalSlice

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1143,7 +1143,6 @@ interface ModalSlice {
   unlockModal: UnlockModal | null
   farmModal: FarmModal | null
   walletAssetsModal: WalletAssetModal | null
-  evmBridgeAssetsModal: EVMBridgeAssetsModal | null
   withdrawFromVaultsModal: DepositedVault[] | null
   v1DepositAndWithdrawModal: V1DepositAndWithdrawModal | null
   v1BorrowAndRepayModal: V1BorrowAndRepayModal | null
