05269fc96ce706723dfeaa146d3a101cabf4c63b
Monkmansteve <sxatzakis@yahoo.gr>
2024-08-22 11:45:15 +0300

    feature: Add SkipClient

--- src/components/account/AccountFund/AccountFundContent.tsx
+++ src/components/account/AccountFund/AccountFundContent.tsx
@@ -21,6 +21,9 @@ import AccountFundingAssets from './AccountFundingAssets'
 import { ArrowRight, Plus } from 'components/common/Icons'
 import Button from 'components/common/Button'
 import getAccountIds from 'api/wallets/getAccountIds'
+import { RecommendationEntry, SkipClient } from '@skip-go/client'
+import { createWalletClient, custom, WalletClient } from 'viem'
+import { CHAIN_NAMES, chainNameToViemChain, USDC_ADDRESSES } from 'utils/fetchUSDCBalance'
 
 interface Props {
   account?: Account
@@ -47,7 +50,10 @@ export default function AccountFundContent(props: Props) {
   }, [walletAssetModal?.selectedDenoms])
   const { fundingAssets, updateFundingAssets, setFundingAssets } = useFundingAssets(selectedDenoms)
   const { depositCapReachedCoins } = useDepositCapCalculations(fundingAssets)
-  const { isConnected, handleDisconnectWallet } = useWeb3WalletConnection()
+  const { isConnected, address: evmAddress, handleDisconnectWallet } = useWeb3WalletConnection()
+  const cosmosAddress = useStore((s) => s.address)
+  const [recommendedRoute, setRecommendedRoute] = useState<RecommendationEntry | null>(null)
+
   const { enableAutoLendForNewAccount } = useAutoLend()
   const hasAssetSelected = fundingAssets.length > 0
   const hasFundingAssets =
@@ -61,6 +67,116 @@ export default function AccountFundContent(props: Props) {
 
   const chainConfig = useChainConfig()
 
+  const [skipClient] = useState(
+    () =>
+      new SkipClient({
+        apiKey: process.env.NEXT_PUBLIC_SKIP_API_KEY,
+        getCosmosSigner: async (chainID) => {
+          const offlineSigner = window.keplr?.getOfflineSigner(chainID)
+          if (!offlineSigner) throw new Error('Keplr not installed')
+          return offlineSigner
+        },
+        getEVMSigner: async (chainName: string) => {
+          const ethereum = window.ethereum
+          if (!ethereum) throw new Error('MetaMask not installed')
+          const client = createWalletClient({
+            chain: chainNameToViemChain[chainName],
+            transport: custom(ethereum),
+          })
+          return client as any
+        },
+      }),
+  )
+
+  const getRecommendedRoute = useCallback(
+    async (selectedAsset: any) => {
+      if (!selectedAsset.chain) return null
+
+      const request = {
+        sourceAssetDenom: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359', // USDC
+        sourceAssetChainID: '137',
+        destChainID: 'neutron-1',
+      }
+
+      try {
+        const recommendations = await skipClient.recommendAssets(request)
+        return recommendations[0]
+      } catch (error) {
+        console.error('Failed to get recommended route:', error)
+        return null
+      }
+    },
+    [skipClient],
+  )
+
+  const CHAIN_IDS = Object.fromEntries(Object.entries(CHAIN_NAMES).map(([id, name]) => [name, id]))
+
+  const handleSkipTransfer = useCallback(async () => {
+    if (!cosmosAddress || !evmAddress || fundingAssets.length === 0) return
+
+    const selectedAsset = fundingAssets[0]
+    console.log(
+      'routeProperties',
+      selectedAsset.coin.amount.toString(),
+      selectedAsset.coin.denom,
+      chainConfig.id,
+      selectedAsset.coin.amount.toString(),
+      selectedAsset.coin.denom,
+      'ethereum',
+    )
+    try {
+      if (!selectedAsset.chain) throw new Error('Chain not found')
+
+      const recommendation = await getRecommendedRoute(selectedAsset)
+      setRecommendedRoute(recommendation)
+
+      if (!recommendation) throw new Error('No recommended route found')
+
+      const route = await skipClient.route({
+        sourceAssetDenom: USDC_ADDRESSES[CHAIN_IDS[selectedAsset.chain]],
+        sourceAssetChainID: CHAIN_IDS[selectedAsset.chain],
+        destAssetDenom: chainConfig.defaultCurrency.coinMinimalDenom,
+        destAssetChainID: chainConfig.id,
+        amountIn: selectedAsset.coin.amount.toString(),
+        cumulativeAffiliateFeeBPS: '0',
+        allowUnsafe: true,
+        experimentalFeatures: ['hyperlane'],
+        allowMultiTx: true,
+        smartRelay: true,
+        smartSwapOptions: {
+          splitRoutes: true,
+          evmSwaps: true,
+        },
+      })
+      const assets = await skipClient.assets({
+        includeEvmAssets: true,
+      })
+      console.log('assets:', assets)
+
+      const userAddresses = await Promise.all(
+        route.requiredChainAddresses.map(async (chainID) => ({
+          chainID,
+          address: chainID === 'neutron-1' ? cosmosAddress : evmAddress,
+        })),
+      )
+
+      await skipClient.executeRoute({
+        route,
+        userAddresses,
+      })
+    } catch (error) {
+      console.error('Skip transfer failed:', error)
+    }
+  }, [
+    cosmosAddress,
+    evmAddress,
+    fundingAssets,
+    chainConfig,
+    CHAIN_IDS,
+    getRecommendedRoute,
+    skipClient,
+  ])
+
   const handleSelectAssetsClick = useCallback(() => {
     useStore.setState({
       walletAssetsModal: {
@@ -72,6 +188,7 @@ export default function AccountFundContent(props: Props) {
   }, [selectedDenoms])
 
   const handleClick = useCallback(async () => {
+    console.log('addresses', cosmosAddress, evmAddress)
     const depositObject = {
       coins: fundingAssets.map((wrappedCoin) => wrappedCoin.coin),
       lend: isLending,
@@ -107,6 +224,8 @@ export default function AccountFundContent(props: Props) {
     props.accountId,
     props.address,
     chainConfig,
+    cosmosAddress,
+    evmAddress,
     props.isFullPage,
     deposit,
     fundingAssets,
@@ -184,6 +303,14 @@ export default function AccountFundContent(props: Props) {
           color={props.isFullPage ? 'tertiary' : undefined}
           size={props.isFullPage ? 'lg' : undefined}
           rightIcon={props.isFullPage ? undefined : <ArrowRight />}
+        />
+        <Button
+          className='w-full mt-4'
+          text='Transfer with Skip'
+          disabled={!hasFundingAssets || isConfirming}
+          showProgressIndicator={isConfirming}
+          onClick={handleSkipTransfer}
+          color='secondary'
         />
       </div>
     </>
--- src/utils/fetchUSDCBalance.ts
+++ src/utils/fetchUSDCBalance.ts
@@ -1,8 +1,9 @@
 import { getBalance } from '@wagmi/core'
 import { config } from 'config/ethereumConfig'
 import { arbitrum, base, celo, mainnet, optimism, polygon, zkSync } from '@wagmi/core/chains'
+import { Chain } from 'viem'
 
-const USDC_ADDRESSES: Record<number, `0x${string}`> = {
+export const USDC_ADDRESSES: Record<number | string, `0x${string}`> = {
   [mainnet.id]: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // Ethereum USDC
   [arbitrum.id]: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // Arbitrum USDC
   [base.id]: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', // Base USDC
@@ -10,6 +11,16 @@ const USDC_ADDRESSES: Record<number, `0x${string}`> = {
   [optimism.id]: '0x0b2c639c533813f4aa9d7837caf62653d097ff85', // OP Mainnet USDC
   [polygon.id]: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // Polygon USDC
   [zkSync.id]: '0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4', // zkSync USDC
+}
+
+export const chainNameToViemChain: Record<string, Chain> = {
+  [mainnet.id]: mainnet,
+  [base.id]: base,
+  [arbitrum.id]: arbitrum,
+  [celo.id]: celo,
+  [optimism.id]: optimism,
+  [polygon.id]: polygon,
+  [zkSync.id]: zkSync,
 }
 
 const CHAIN_IDS = [mainnet.id, base.id, arbitrum.id, celo.id, optimism.id, polygon.id, zkSync.id]
