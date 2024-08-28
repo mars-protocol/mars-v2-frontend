1df28e5496831622f1576734d858d813274b6294
Monkmansteve <sxatzakis@yahoo.gr>
2024-08-28 12:55:29 +0300

    feature: implement toast

--- src/components/account/AccountFund/AccountFundContent.tsx
+++ src/components/account/AccountFund/AccountFundContent.tsx
@@ -4,7 +4,6 @@ import DepositCapMessage from 'components/common/DepositCapMessage'
 import SwitchAutoLend from 'components/common/Switch/SwitchAutoLend'
 import Text from 'components/common/Text'
 import { useUpdatedAccount } from 'hooks/accounts/useUpdatedAccount'
-import useBaseAsset from 'hooks/assets/useBasetAsset'
 import useAutoLend from 'hooks/wallet/useAutoLend'
 import useWalletBalances from 'hooks/wallet/useWalletBalances'
 import useStore from 'store'
@@ -16,8 +15,8 @@ import { useFundingAssets } from 'hooks/assets/useFundingAssets'
 import useChainConfig from 'hooks/chain/useChainConfig'
 import { useDepositCapCalculations } from 'hooks/markets/useDepositCapCalculations'
 import { useWeb3WalletConnection } from 'hooks/wallet/useWeb3WalletConnections'
-import EVMAccountSection from './EVMAccountSection'
-import AccountFundingAssets from './AccountFundingAssets'
+import EVMAccountSection from 'components/account/AccountFund/EVMAccountSection'
+import AccountFundingAssets from 'components/account/AccountFund/AccountFundingAssets'
 import { ArrowRight, Plus } from 'components/common/Icons'
 import Button from 'components/common/Button'
 import getAccountIds from 'api/wallets/getAccountIds'
@@ -26,6 +25,8 @@ import { WalletClient } from 'viem'
 import { CHAIN_NAMES, chainNameToUSDCAttributes } from 'utils/fetchUSDCBalance'
 import { getWalletClient } from '@wagmi/core'
 import { config } from 'config/ethereumConfig'
+import { toast } from 'react-toastify'
+import useBaseAsset from 'hooks/assets/useBasetAsset'
 
 interface Props {
   account?: Account
@@ -90,60 +91,75 @@ export default function AccountFundContent(props: Props) {
     [],
   )
 
-  const handleSkipTransfer = useCallback(async () => {
-    if (!cosmosAddress || !evmAddress || fundingAssets.length === 0) {
-      console.error('Missing required data for transfer')
-      return
-    }
-
-    const selectedAsset = fundingAssets[0]
-
-    try {
-      if (!selectedAsset.chain) throw new Error('Chain not found for selected asset')
-
-      const route = await skipClient.route({
-        allowMultiTx: true,
-        allowUnsafe: true,
-        cumulativeAffiliateFeeBPS: '0',
-        experimentalFeatures: ['hyperlane'],
-        smartRelay: true,
-        smartSwapOptions: {
-          splitRoutes: true,
-          evmSwaps: true,
-        },
-        sourceAssetDenom: chainNameToUSDCAttributes[selectedAsset.chain].assetAddress,
-        sourceAssetChainID: chainNameToUSDCAttributes[selectedAsset.chain].chainID.toString(),
-        destAssetDenom: 'ibc/B559A80D62249C8AA07A380E2A2BEA6E5CA9A6F079C912C3A9E9B494105E4F81',
-        destAssetChainID: 'neutron-1',
-        amountIn: selectedAsset.coin.amount.toString(),
-      })
-
-      console.log('Received route:', route)
-
-      const userAddresses = route.requiredChainAddresses.map((chainID) => ({
-        chainID,
-        address: chainID === 'neutron-1' || chainID === 'osmosis-1' ? cosmosAddress : evmAddress,
-      }))
-
-      console.log('User addresses:', userAddresses)
-
-      console.log('Executing route...')
-      await skipClient.executeRoute({
-        route,
-        userAddresses,
-        onTransactionCompleted: async (chainID, txHash, status) => {
-          console.log('Transaction completed:', { chainID, txHash, status })
-        },
-      })
-      console.log('Route execution completed')
-    } catch (error) {
-      console.error('Skip transfer failed:', error)
-      if (error instanceof Error) {
-        console.error('Error message:', error.message)
-        console.error('Error stack:', error.stack)
+  const wrapTransactionPromise = (promise: Promise<void>): Promise<BroadcastResult> => {
+    return promise.then(
+      () => ({ success: true }) as BroadcastResult,
+      (error) => ({ success: false, error }) as BroadcastResult,
+    )
+  }
+  const handleSkipTransfer = useCallback(
+    async (selectedAsset: WrappedBNCoin) => {
+      if (!cosmosAddress || !evmAddress || !selectedAsset) {
+        console.error('Missing required data for transfer')
+        return
       }
-    }
-  }, [cosmosAddress, evmAddress, fundingAssets, skipClient])
+
+      try {
+        if (!selectedAsset.chain) throw new Error('Chain not found for selected asset')
+
+        const route = await skipClient.route({
+          allowMultiTx: true,
+          allowUnsafe: true,
+          cumulativeAffiliateFeeBPS: '0',
+          experimentalFeatures: ['hyperlane'],
+          smartRelay: true,
+          smartSwapOptions: {
+            splitRoutes: true,
+            evmSwaps: true,
+          },
+          sourceAssetDenom: chainNameToUSDCAttributes[selectedAsset.chain].assetAddress,
+          sourceAssetChainID: chainNameToUSDCAttributes[selectedAsset.chain].chainID.toString(),
+          destAssetDenom: 'ibc/B559A80D62249C8AA07A380E2A2BEA6E5CA9A6F079C912C3A9E9B494105E4F81',
+          destAssetChainID: 'neutron-1',
+          amountIn: selectedAsset.coin.amount.toString(),
+        })
+
+        console.log('Received route:', route)
+
+        const userAddresses = route.requiredChainAddresses.map((chainID) => ({
+          chainID,
+          address: chainID === 'neutron-1' || chainID === 'osmosis-1' ? cosmosAddress : evmAddress,
+        }))
+
+        console.log('User addresses:', userAddresses)
+
+        console.log('Executing route...')
+        const transactionPromise = skipClient.executeRoute({
+          route,
+          userAddresses,
+          onTransactionCompleted: async (chainID, txHash, status) => {
+            console.log('Transaction completed:', { chainID, txHash, status })
+          },
+        })
+        const wrappedTransactionPromise = wrapTransactionPromise(transactionPromise)
+
+        useStore.getState().handleTransaction({
+          response: wrappedTransactionPromise,
+          message: 'EVM transaction in progress...',
+        })
+
+        await wrappedTransactionPromise
+        console.log('Route execution completed')
+      } catch (error) {
+        console.error('Skip transfer failed:', error)
+        if (error instanceof Error) {
+          console.error('Error message:', error.message)
+          console.error('Error stack:', error.stack)
+        }
+      }
+    },
+    [cosmosAddress, evmAddress, skipClient],
+  )
 
   const handleSelectAssetsClick = useCallback(() => {
     useStore.setState({
@@ -164,18 +180,33 @@ export default function AccountFundContent(props: Props) {
 
     setIsConfirming(true)
     let result
-    if (props.accountId) {
-      result = await deposit({ ...depositObject, accountId: props.accountId })
-    } else {
-      result = await deposit(depositObject)
-      if (result) {
-        const accountIds = await getAccountIds(chainConfig, props.address)
-        if (accountIds.length > 0) {
-          const latestAccountId = accountIds[accountIds.length - 1].id
-          enableAutoLendForNewAccount(latestAccountId)
+
+    const evmAssets = fundingAssets.filter(
+      (asset) => asset.chain && chainNameToUSDCAttributes[asset.chain],
+    )
+    const nonEvmAssets = fundingAssets.filter(
+      (asset) => !asset.chain || !chainNameToUSDCAttributes[asset.chain],
+    )
+
+    if (evmAssets.length > 0) {
+      await handleSkipTransfer(evmAssets[0])
+    }
+
+    if (nonEvmAssets.length > 0) {
+      if (props.accountId) {
+        result = await deposit({ ...depositObject, accountId: props.accountId })
+      } else {
+        result = await deposit(depositObject)
+        if (result) {
+          const accountIds = await getAccountIds(chainConfig, props.address)
+          if (accountIds.length > 0) {
+            const latestAccountId = accountIds[accountIds.length - 1].id
+            enableAutoLendForNewAccount(latestAccountId)
+          }
         }
       }
     }
+
     setIsConfirming(false)
 
     if (result) {
@@ -199,6 +230,7 @@ export default function AccountFundContent(props: Props) {
     fundingAssets,
     isLending,
     enableAutoLendForNewAccount,
+    handleSkipTransfer,
   ])
 
   useEffect(() => {
@@ -271,14 +303,6 @@ export default function AccountFundContent(props: Props) {
           color={props.isFullPage ? 'tertiary' : undefined}
           size={props.isFullPage ? 'lg' : undefined}
           rightIcon={props.isFullPage ? undefined : <ArrowRight />}
-        />
-        <Button
-          className='w-full mt-4'
-          text='Transfer with Skip'
-          disabled={!hasFundingAssets || isConfirming}
-          showProgressIndicator={isConfirming}
-          onClick={handleSkipTransfer}
-          color='secondary'
         />
       </div>
     </>
