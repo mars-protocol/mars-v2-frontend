55f33b208f5787ff80a125ce8ff5ed43aafcc3bc
Patricie <patricie.sm@gmail.com>
2024-08-28 09:37:45 +0200

    feat: callout warning for slippage & price impact

--- src/components/trade/TradeModule/SwapForm/TradeSummary.tsx
+++ src/components/trade/TradeModule/SwapForm/TradeSummary.tsx
@@ -1,11 +1,14 @@
 import classNames from 'classnames'
 
 import ActionButton from 'components/common/Button/ActionButton'
+import { Callout, CalloutType } from 'components/common/Callout'
 import { CircularProgress } from 'components/common/CircularProgress'
 import Divider from 'components/common/Divider'
 import { FormattedNumber } from 'components/common/FormattedNumber'
 import { RouteInfo } from 'components/common/RouteInfo'
 import SummaryLine from 'components/common/SummaryLine'
+import Text from 'components/common/Text'
+import { Tooltip } from 'components/common/Tooltip'
 import { BN_ZERO } from 'constants/math'
 import useLiquidationPrice from 'hooks/prices/useLiquidationPrice'
 import useSlippage from 'hooks/settings/useSlippage'
@@ -60,68 +63,81 @@ export default function TradeSummary(props: Props) {
   }, [isAdvanced, direction, sellAsset.symbol, buyAsset.symbol])
 
   return (
-    <div
-      className={classNames(
-        containerClassName,
-        'flex flex-1 flex-col bg-white/5 rounded border border-white/20',
+    <div className='flex flex-col w-full space-y-2'>
+      {routeInfo?.priceImpact && routeInfo.priceImpact.toNumber() > 1 && (
+        <Callout type={CalloutType.WARNING}>
+          The price impact of your trade is above 1%. Please review before submitting the
+          transaction.
+        </Callout>
       )}
-    >
-      <div className='flex flex-col flex-1 m-3'>
-        <SummaryLine label='Liquidation Price'>
-          <div className='flex h-2'>
-            {isUpdatingLiquidationPrice ? (
-              <CircularProgress size={10} />
-            ) : liquidationPrice === null || liquidationPrice === 0 ? (
-              '-'
-            ) : (
-              <FormattedNumber
-                className='inline'
-                amount={liquidationPrice}
-                options={{ abbreviated: true, prefix: `${props.buyAsset.symbol} = $` }}
-              />
-            )}
-          </div>
-        </SummaryLine>
-        <Divider className='my-2' />
-        {isMargin && (
-          <>
-            <SummaryLine label='Borrowing'>
-              <FormattedNumber
-                amount={borrowAmount.toNumber()}
-                options={{
-                  decimals: sellAsset.decimals,
-                  maxDecimals: sellAsset.decimals,
-                  minDecimals: 0,
-                  suffix: ` ${sellAsset.symbol}`,
-                  abbreviated: true,
-                  rounded: true,
-                }}
-              />
-            </SummaryLine>
-            <SummaryLine label='Borrow Rate APY'>
-              <span>{formatPercent(borrowRate || 0)}</span>
-            </SummaryLine>
-            <Divider className='my-2' />
-          </>
+      {routeInfo && slippage * 100 > 1 && (
+        <Callout type={CalloutType.WARNING}>
+          The slippage of your trade is above 1%. Please review before submitting the transaction.
+        </Callout>
+      )}
+      <div
+        className={classNames(
+          containerClassName,
+          'flex flex-1 flex-col bg-white/5 rounded border border-white/20',
         )}
-        {routeInfo && (
-          <RouteInfo
-            title='Summary'
-            route={routeInfo}
-            assets={{ in: sellAsset, out: buyAsset }}
-            tradeInfo={{ slippage, minReceive }}
-          />
-        )}
+      >
+        <div className='flex flex-col flex-1 m-3'>
+          <SummaryLine label='Liquidation Price'>
+            <div className='flex h-2'>
+              {isUpdatingLiquidationPrice ? (
+                <CircularProgress size={10} />
+              ) : liquidationPrice === null || liquidationPrice === 0 ? (
+                '-'
+              ) : (
+                <FormattedNumber
+                  className='inline'
+                  amount={liquidationPrice}
+                  options={{ abbreviated: true, prefix: `${props.buyAsset.symbol} = $` }}
+                />
+              )}
+            </div>
+          </SummaryLine>
+          <Divider className='my-2' />
+          {isMargin && (
+            <>
+              <SummaryLine label='Borrowing'>
+                <FormattedNumber
+                  amount={borrowAmount.toNumber()}
+                  options={{
+                    decimals: sellAsset.decimals,
+                    maxDecimals: sellAsset.decimals,
+                    minDecimals: 0,
+                    suffix: ` ${sellAsset.symbol}`,
+                    abbreviated: true,
+                    rounded: true,
+                  }}
+                />
+              </SummaryLine>
+              <SummaryLine label='Borrow Rate APY'>
+                <span>{formatPercent(borrowRate || 0)}</span>
+              </SummaryLine>
+              <Divider className='my-2' />
+            </>
+          )}
+          {routeInfo && (
+            <RouteInfo
+              title='Summary'
+              route={routeInfo}
+              assets={{ in: sellAsset, out: buyAsset }}
+              tradeInfo={{ slippage, minReceive }}
+            />
+          )}
+        </div>
+        <ActionButton
+          disabled={buyButtonDisabled}
+          showProgressIndicator={showProgressIndicator}
+          text={buttonText}
+          onClick={buyAction}
+          size='md'
+          color='primary'
+          className='w-full'
+        />
       </div>
-      <ActionButton
-        disabled={buyButtonDisabled}
-        showProgressIndicator={showProgressIndicator}
-        text={buttonText}
-        onClick={buyAction}
-        size='md'
-        color='primary'
-        className='w-full'
-      />
     </div>
   )
 }
