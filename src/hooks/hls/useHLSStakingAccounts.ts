8fa57454b1150d9e7b1c8d35f17b20b73ac5fb1d
Linkie Link <linkielink.dev@gmail.com>
2024-09-02 18:24:37 +0200

    fix: fixed the build

--- src/hooks/hls/useHLSStakingAccounts.ts
+++ src/hooks/hls/useHLSStakingAccounts.ts
@@ -2,15 +2,17 @@ import useSWR from 'swr'
 
 import getHLSStakingAccounts from 'api/hls/getHLSStakingAccounts'
 import useWhitelistedAssets from 'hooks/assets/useWhitelistedAssets'
+import useCampaignApys from 'hooks/campaign/useCampaignApys'
 import useChainConfig from 'hooks/chain/useChainConfig'
 
 export default function useHLSStakingAccounts(address?: string) {
   const chainConfig = useChainConfig()
   const assets = useWhitelistedAssets()
+  const { data: apys } = useCampaignApys()
 
   return useSWR(
     `${address}/hlsStakingAccounts`,
-    () => getHLSStakingAccounts(chainConfig, assets, address),
+    () => getHLSStakingAccounts(chainConfig, assets, apys, address),
     {
       fallbackData: [],
       suspense: true,
