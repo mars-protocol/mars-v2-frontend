5b5ae9f9adc1a4f5116c31274f3723b0a022b86d
Patricie <patricie.sm@gmail.com>
2024-09-04 12:27:34 +0200

    feat: add info modal

--- src/components/vaults/community/VaultsCommunityIntro.tsx
+++ src/components/vaults/community/VaultsCommunityIntro.tsx
@@ -1,10 +1,51 @@
 import Button from 'components/common/Button'
 import ActionButton from 'components/common/Button/ActionButton'
-import { Plus, PlusSquared } from 'components/common/Icons'
+import { Account, ArrowRight, HandCoins, Plus, PlusSquared, Wallet } from 'components/common/Icons'
 import Intro from 'components/common/Intro'
+import { AlertDialogItems } from 'components/Modals/AlertDialog/AlertDialogItems'
+import { LocalStorageKeys } from 'constants/localStorageKeys'
+import useAlertDialog from 'hooks/common/useAlertDialog'
+import useLocalStorage from 'hooks/localStorage/useLocalStorage'
+import { useCallback } from 'react'
 import { DocURL } from 'types/enums'
 
 export default function VaultsCommunityIntro() {
+  const [showVaultInformation, setShowVaultInformation] = useLocalStorage<boolean>(
+    LocalStorageKeys.VAULT_COMMUNITY_INFORMATION,
+    true,
+  )
+  const { open: showAlertDialog, close } = useAlertDialog()
+
+  const handleOnClick = useCallback(() => {
+    if (!showVaultInformation) {
+      // TODO
+      // opencreatevaultmodal
+      return
+    }
+
+    showAlertDialog({
+      title: 'Become a Vault Manager',
+      content: <AlertDialogItems items={INFO_ITEMS} />,
+
+      positiveButton: {
+        text: 'Continue',
+        icon: <ArrowRight />,
+        onClick: () => {},
+      },
+      negativeButton: {
+        text: 'Cancel',
+        onClick: () => {
+          setShowVaultInformation(true)
+          close()
+        },
+      },
+      checkbox: {
+        text: "Don't show again",
+        onClick: (isChecked: boolean) => setShowVaultInformation(!isChecked),
+      },
+    })
+  }, [close, showAlertDialog, showVaultInformation, setShowVaultInformation])
+
   return (
     <Intro
       bg='vaults-community'
@@ -16,7 +57,12 @@ export default function VaultsCommunityIntro() {
         </>
       }
     >
-      <ActionButton text='Create Vault' color='primary' leftIcon={<Plus />} onClick={() => {}} />
+      <ActionButton
+        text='Create Vault'
+        color='primary'
+        leftIcon={<Plus />}
+        onClick={handleOnClick}
+      />
       <Button
         text='Learn more'
         leftIcon={<PlusSquared />}
@@ -29,3 +75,24 @@ export default function VaultsCommunityIntro() {
     </Intro>
   )
 }
+
+const INFO_ITEMS = [
+  {
+    icon: <Account />,
+    title: 'Create a vault',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum arcu enim, cursus vel nulla quis, euismod molestie est.',
+  },
+  {
+    icon: <HandCoins />,
+    title: 'Manage funds',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum arcu enim, cursus vel nulla quis, euismod molestie est.',
+  },
+  {
+    icon: <Wallet />,
+    title: 'Earn with performance fees',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum arcu enim, cursus vel nulla quis, euismod molestie est.',
+  },
+]
