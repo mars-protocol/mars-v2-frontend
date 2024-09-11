bcc82d6ca4f35320580e0d3cc74b6b992418eb27
Linkie Link <linkielink.dev@gmail.com>
2024-09-11 17:17:33 +0200

    feat: change the way caps are fetched

--- src/hooks/markets/useMarketDepositCaps.ts
+++ src/hooks/markets/useMarketDepositCaps.ts
@@ -1,13 +1,30 @@
 import useSWR from 'swr'
 
-import useWhitelistedAssets from 'hooks/assets/useWhitelistedAssets'
+import useAssets from 'hooks/assets/useAssets'
 import useChainConfig from 'hooks/chain/useChainConfig'
 import useClients from 'hooks/chain/useClients'
+import useAssetParams from 'hooks/params/useAssetParams'
+import { useMemo } from 'react'
+import { byDenom } from 'utils/array'
 
 export default function useMarketDepositCaps() {
   const chainConfig = useChainConfig()
   const clients = useClients()
-  const assets = useWhitelistedAssets()
+  const { data: assetParams } = useAssetParams()
+  const { data: allAssets } = useAssets()
+  const assets = useMemo(() => {
+    const assetsWithParams = [] as Asset[]
+
+    if (!allAssets || !assetParams) return assetsWithParams
+
+    assetParams.forEach((params) => {
+      const asset = allAssets.find(byDenom(params.denom))
+      if (!asset) return
+      assetsWithParams.push(asset)
+    })
+
+    return assetsWithParams
+  }, [allAssets, assetParams])
 
   return useSWR(
     assets.length > 0 && clients && `chains/${chainConfig.id}/markets/depositCap`,
