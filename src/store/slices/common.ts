bda95378b5a3f11afc9608bd408c754ada8b9b61
Patricie <patricie.sm@gmail.com>
2024-09-11 17:02:21 +0200

    tidy: cleanup

--- src/store/slices/common.ts
+++ src/store/slices/common.ts
@@ -21,7 +21,6 @@ export default function createCommonSlice(
     isOracleStale: false,
     isHls: false,
     isVaults: false,
-    isVaultsCreate: false,
     isV1: false,
     assets: [],
     hlsBorrowAmount: null,
