29eed10777c8d2ef40277f99f4017e2c7aaf951f
Patricie <patricie.sm@gmail.com>
2024-09-24 13:32:52 +0200

    feat: banner for perps deposits

---
+++ src/components/common/Banner.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames'
+import Text from 'components/common/Text'
+import React from 'react'
+
+interface Props {
+  icon: React.ReactNode
+  title: React.ReactNode
+  description: string
+  button: React.ReactNode
+}
+
+export default function Banner(props: Props) {
+  const { icon, title, description, button } = props
+
+  return (
+    <div
+      className={classNames(
+        'flex justify-between items-center w-full px-8 bg-white/10 mb-4 h-20',
+        'relative isolate overflow-hidden rounded-base',
+        'before:content-[" "] before:absolute before:inset-0 before:-z-1 before:rounded-base before:p-[1px] before:border-glas',
+      )}
+    >
+      <div
+        className={classNames(
+          'absolute h-[20vw] w-[40vw] bottom-[0] right-[-10vw] rounded-full opacity-30',
+          'blur-orb-secondary bg-purple',
+        )}
+      />
+      <div className='flex gap-4 items-center'>
+        <span className='w-10 h-10'>{icon}</span>
+        <div className='space-y-2'>
+          {title}
+          <Text size='sm' className='text-white/50'>
+            {description}
+          </Text>
+        </div>
+      </div>
+      {button}
+    </div>
+  )
+}
--- src/pages/PerpsPage.tsx
+++ src/pages/PerpsPage.tsx
@@ -1,11 +1,31 @@
+import Banner from 'components/common/Banner'
+import Text from 'components/common/Text'
+import usePerpsVault from 'hooks/perps/usePerpsVault'
+import { USDC } from 'components/common/Icons'
 import { PerpsModule } from 'components/perps/Module/PerpsModule'
 import { PerpsChart } from 'components/perps/PerpsChart'
 import { PerpsPositions } from 'components/perps/PerpsPositions'
+import { Deposit } from 'components/earn/farm/common/Table/Columns/Deposit'
 
 export default function PerpsPage() {
+  const { data: vault } = usePerpsVault()
+
   return (
     <div className='md:grid flex flex-wrap w-full md:grid-cols-[auto_346px] gap-4'>
       <div>
+        <Banner
+          icon={<USDC />}
+          title={
+            <Text>
+              Deposit in the counter-party vault, earn up to{' '}
+              <span className='text-purple'>5% APY.</span>
+            </Text>
+          }
+          description='Earn Perps Trading Fees by depositing USDC into the counter-party vault. Note: Deposits are subject to 7 days lockup period.'
+          button={
+            <Deposit vault={vault as PerpsVault} isLoading={false} isPerps buttonColor='primary' />
+          }
+        />
         <PerpsChart />
       </div>
       <div className='row-span-2'>
