3f400ed94df3b00098f651a3b7620c369cb12966
Linkie Link <linkielink.dev@gmail.com>
2024-09-24 21:21:44 +0200

    fix: TradeDirection positioning

--- src/components/perps/BalancesTable/Columns/TradeDirection.tsx
+++ src/components/perps/BalancesTable/Columns/TradeDirection.tsx
@@ -14,14 +14,14 @@ type Props = {
 export default function TradeDirection(props: Props) {
   const { tradeDirection, className, previousTradeDirection } = props
   return (
-    <div className='flex items-center gap-1'>
+    <div className='inline-flex items-center gap-1'>
       {previousTradeDirection && (
         <>
           <Text
             size='xs'
             tag='div'
             className={classNames(
-              'capitalize px-2 py-0.5 rounded-sm inline-block flex items-center',
+              'capitalize px-2 py-0.5 rounded-sm flex items-center',
               previousTradeDirection === 'short' && 'text-error bg-error/20',
               previousTradeDirection === 'long' && 'text-success bg-success/20',
               className,
@@ -44,7 +44,7 @@ export default function TradeDirection(props: Props) {
         size='xs'
         tag='div'
         className={classNames(
-          'capitalize px-2 py-0.5 rounded-sm inline-block flex items-center',
+          'capitalize px-2 py-0.5 rounded-sm flex items-center',
           tradeDirection === 'short' && 'text-error bg-error/20',
           tradeDirection === 'long' && 'text-success bg-success/20',
           className,
