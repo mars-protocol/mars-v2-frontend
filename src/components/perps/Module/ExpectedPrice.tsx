b5af27c5477c2594adcad7dc5c32833ce0c489a8
Linkie Link <linkielink.dev@gmail.com>
2024-09-24 20:12:30 +0200

    fix: ExpectedPrice

--- src/components/perps/Module/ExpectedPrice.tsx
+++ src/components/perps/Module/ExpectedPrice.tsx
@@ -2,8 +2,11 @@ import BigNumber from 'bignumber.js'
 
 import { CircularProgress } from 'components/common/CircularProgress'
 import DisplayCurrency from 'components/common/DisplayCurrency'
+import { PRICE_ORACLE_DECIMALS } from 'constants/query'
+import usePerpsEnabledAssets from 'hooks/assets/usePerpsEnabledAssets'
 import useTradingFeeAndPrice from 'hooks/perps/useTradingFeeAndPrice'
 import { BNCoin } from 'types/classes/BNCoin'
+import { byDenom } from 'utils/array'
 
 type Props = {
   denom: string
@@ -12,22 +15,22 @@ type Props = {
 }
 
 export const ExpectedPrice = (props: Props) => {
+  const perpsAssets = usePerpsEnabledAssets()
   const { data: tradingFeeAndPrice, isLoading } = useTradingFeeAndPrice(
     props.denom,
     props.newAmount,
   )
-
+  const perpsAsset = perpsAssets.find(byDenom(props.denom))
   if (isLoading) return <CircularProgress className='h-full' size={12} />
+  if (!tradingFeeAndPrice?.price || !perpsAsset) return '-'
 
-  if (tradingFeeAndPrice?.price) {
-    return (
-      <DisplayCurrency
-        coin={BNCoin.fromDenomAndBigNumber('usd', tradingFeeAndPrice.price)}
-        options={{ maxDecimals: tradingFeeAndPrice.price.isGreaterThan(100) ? 2 : 6 }}
-        className={props.className}
-      />
-    )
-  }
+  const price = tradingFeeAndPrice.price.shiftedBy(perpsAsset.decimals - PRICE_ORACLE_DECIMALS)
 
-  return '-'
+  return (
+    <DisplayCurrency
+      coin={BNCoin.fromDenomAndBigNumber('usd', price)}
+      options={{ maxDecimals: price.isGreaterThan(100) ? 2 : 6, abbreviated: false }}
+      className={props.className}
+    />
+  )
 }
