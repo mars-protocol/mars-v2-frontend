ba34fe7671faabea86c4553f42c873b6eb23ae66
Linkie Link <linkielink.dev@gmail.com>
2024-09-26 12:37:19 +0200

    fix: APY issue on banner

--- src/components/perps/PerpsBanner.tsx
+++ src/components/perps/PerpsBanner.tsx
@@ -3,6 +3,7 @@ import { Deposit } from 'components/earn/farm/common/Table/Columns/Deposit'
 import useWhitelistedAssets from 'hooks/assets/useWhitelistedAssets'
 import usePerpsVault from 'hooks/perps/usePerpsVault'
 import { byDenom } from 'utils/array'
+import { formatValue } from 'utils/formatters'
 
 export default function PerpsBanner() {
   const { data: vault } = usePerpsVault()
@@ -14,7 +15,15 @@ export default function PerpsBanner() {
       asset={asset}
       title={
         <>
-          Counterparty vault: earn up to <span className='text-purple'>{vault?.apy}% APY.</span>
+          Counterparty vault: earn up to{' '}
+          <span className='text-purple'>
+            {formatValue(vault?.apy ?? 0, {
+              suffix: '% APY',
+              maxDecimals: 2,
+              minDecimals: 0,
+              abbreviated: false,
+            })}
+          </span>
         </>
       }
       description={`Earn perps trading fees by depositing ${asset?.symbol} into the counterparty vault, with deposits subject to a ${vault?.lockup.duration}-${vault?.lockup.timeframe} lockup.`}
