bfed5b3f12300da3c7f858881b9c7b59eccf664d
Linkie Link <linkielink.dev@gmail.com>
2024-10-02 10:04:47 +0200

    feat: create fetchWithTimeout

--- src/api/gasPrice/getGasPrice.ts
+++ src/api/gasPrice/getGasPrice.ts
@@ -1,4 +1,7 @@
+import { FETCH_TIMEOUT } from 'constants/query'
 import { ChainInfoID } from 'types/enums'
+import { setNodeError } from 'utils/error'
+import { fetchWithTimeout } from 'utils/fetch'
 
 export default async function getGasPrice(chainConfig: ChainConfig) {
   const { id, endpoints, gasPrice, defaultCurrency } = chainConfig
@@ -11,7 +14,7 @@ export default async function getGasPrice(chainConfig: ChainConfig) {
       : new URL(endpoints.gasPrices, endpoints.rest)
 
   try {
-    const response = await fetch(uri.toString())
+    const response = await fetchWithTimeout(uri.toString(), FETCH_TIMEOUT)
     const data = await response.json()
 
     const price =
@@ -21,7 +24,7 @@ export default async function getGasPrice(chainConfig: ChainConfig) {
 
     return `${price}${defaultCurrency.coinMinimalDenom}`
   } catch (e) {
-    console.error('Failed to fetch gas price:', e)
+    setNodeError(uri.toString(), 'Failed to fetch gas prices')
     return gasPrice
   }
 }
