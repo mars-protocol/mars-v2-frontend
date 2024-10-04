3738866bcb5e96433e4184e28e74890092174d06
Monkmansteve <sxatzakis@yahoo.gr>
2024-10-04 16:08:25 +0300

    feature: fix failed builds and address comments

--- src/constants/math.ts
+++ src/constants/math.ts
@@ -2,6 +2,7 @@ import { BN } from 'utils/helpers'
 
 export const BN_ZERO = BN(0)
 export const BN_ONE = BN(1)
+export const BN_ZERO_ONE = BN(0.1)
 
 export const MIN_AMOUNT = 0.000001
 export const MAX_AMOUNT_DECIMALS = 6
