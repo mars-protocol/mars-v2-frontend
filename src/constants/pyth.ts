ea78b23cbde0cc8fb2698fd145217c1efd680af1
Monkmansteve <sxatzakis@yahoo.gr>
2024-10-07 21:16:27 +0300

    test: test new rpc endpojt

--- src/constants/pyth.ts
+++ src/constants/pyth.ts
@@ -1,4 +1,7 @@
 export const pythEndpoints = {
-  api: process.env.NEXT_PUBLIC_PYTH_API ?? 'https://hermes.pyth.network/api',
+  // api: process.env.NEXT_PUBLIC_PYTH_API ?? 'https://hermes.pyth.network/api',
+  api:
+    process.env.NEXT_PUBLIC_PYTH_API ??
+    'https://gateway-lon.liquify.com/api=HERMES1J8SML6CBGD673H9/api',
   candles: 'https://benchmarks.pyth.network/v1/shims/tradingview',
 }
