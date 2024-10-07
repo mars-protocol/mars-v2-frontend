f740e4495946b63f0f2338a917ba00fd2e13ccd2
Monkmansteve <sxatzakis@yahoo.gr>
2024-10-07 21:26:27 +0300

    feature: use env value

--- src/constants/pyth.ts
+++ src/constants/pyth.ts
@@ -1,7 +1,5 @@
 export const pythEndpoints = {
   // api: process.env.NEXT_PUBLIC_PYTH_API ?? 'https://hermes.pyth.network/api',
-  api:
-    process.env.NEXT_PUBLIC_PYTH_API ??
-    'https://gateway-lon.liquify.com/api=HERMES1J8SML6CBGD673H9/api',
+  api: process.env.NEXT_PUBLIC_PYTH_TEST_API ?? 'https://hermes.pyth.network/api',
   candles: 'https://benchmarks.pyth.network/v1/shims/tradingview',
 }
