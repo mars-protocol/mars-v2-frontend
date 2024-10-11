67b5dfc4bb0c1308003e56129c46632657094123
Patricie <patricie.sm@gmail.com>
2024-10-11 21:41:21 +0100

    refactor: pr comments & textarea into component

--- src/components/vaults/community/createVault/PerformanceFee.tsx
+++ src/components/vaults/community/createVault/PerformanceFee.tsx
@@ -1,36 +1,50 @@
 import Button from 'components/common/Button'
-import VaultInputElement from 'components/vaults/community/createVault/VaultInputElement'
 import classNames from 'classnames'
+import NumberInput from 'components/common/NumberInput'
+import Text from 'components/common/Text'
 import { Callout, CalloutType } from 'components/common/Callout'
 import { useState } from 'react'
+import { TextLink } from 'components/common/TextLink'
+import { ExternalLink } from 'components/common/Icons'
+import { BN } from 'utils/helpers'
+import { BN_ZERO } from 'constants/math'
 
 const fees = [
-  { label: '1%', value: '1' },
-  { label: '2%', value: '2' },
-  { label: '5%', value: '5' },
-  { label: '10%', value: '10' },
-  { label: '15%', value: '15' },
-  { label: '20%', value: '20' },
+  { label: '1%', value: BN(1) },
+  { label: '2%', value: BN(2) },
+  { label: '5%', value: BN(5) },
+  { label: '10%', value: BN(10) },
+  { label: '15%', value: BN(15) },
+  { label: '20%', value: BN(20) },
 ]
 
 export default function PerformanceFee() {
-  const [feeValue, setFeeValue] = useState<string>('2')
+  const [feeValue, setFeeValue] = useState<BigNumber>(BN(1))
 
-  const handleFeeClick = (fee: string, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleFeeClick = (
+    fee: BigNumber,
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
     event.preventDefault()
     setFeeValue(fee)
   }
 
   return (
     <div className='w-full mb-6 space-y-3'>
-      {/* TODO: add logic for max number for fees */}
-      <VaultInputElement
-        type='text'
-        value={feeValue}
+      <Text size='xs' className='flex items-center'>
+        Specify your performance fee
+      </Text>
+      <NumberInput
+        amount={feeValue}
         onChange={(value) => setFeeValue(value)}
-        label='Specify your performance fee'
+        asset={{ decimals: 0, symbol: '%' }}
+        maxDecimals={0}
+        min={BN_ZERO}
+        max={BN(50)}
+        maxLength={2}
         suffix='%'
         placeholder='Enter fee'
+        className='px-4 py-3 rounded-sm bg-white/5 !border-solid border border-white/10 focus:border-white/20 focus:bg-white/10 !text-left'
       />
       <div className='flex gap-2 justify-evenly'>
         {fees.map((fee, index) => (
@@ -39,7 +53,7 @@ export default function PerformanceFee() {
             variant='solid'
             color='tertiary'
             size='sm'
-            className={classNames('w-full min-w-0', feeValue === fee.value && 'bg-white/20')}
+            className={classNames('w-full min-w-0', feeValue.isEqualTo(fee.value) && 'bg-white/20')}
             text={fee.label}
             key={index}
           />
@@ -47,17 +61,17 @@ export default function PerformanceFee() {
       </div>
       <Callout type={CalloutType.INFO}>
         Performance fees are capped at 50%.
-        <span className='inline-block'>
-          <Button
-            // TODO: add link
-            onClick={() => {}}
-            variant='transparent'
-            color='quaternary'
-            size='xs'
-            className='hover:underline'
-            text='Learn more'
-          />
-        </span>
+        {/* TODO add link */}
+        <TextLink
+          href={''}
+          target='_blank'
+          textSize='extraSmall'
+          className='ml-1 text-white/50 hover:text-white'
+          title='Performance fees'
+        >
+          Learn more
+          <ExternalLink className='ml-1 inline w-3' />
+        </TextLink>
       </Callout>
     </div>
   )
