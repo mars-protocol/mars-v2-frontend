e128fc2895f8592a6d871917adad9366f439cd08
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2024-10-16 17:28:14 +0200

    improve: perp market state

--- src/hooks/health-computer/useHealthComputer.ts
+++ src/hooks/health-computer/useHealthComputer.ts
@@ -4,7 +4,7 @@ import useAssets from 'hooks/assets/useAssets'
 import usePerpsEnabledAssets from 'hooks/assets/usePerpsEnabledAssets'
 import useWhitelistedAssets from 'hooks/assets/useWhitelistedAssets'
 import useAssetParams from 'hooks/params/useAssetParams'
-import useAllPerpsMarketStates from 'hooks/perps/usePerpsMarketStates'
+import usePerpsMarketStates from 'hooks/perps/usePerpsMarketStates'
 import { useAllPerpsParamsSC } from 'hooks/perps/usePerpsParams'
 import usePerpsVault from 'hooks/perps/usePerpsVault'
 import useSlippage from 'hooks/settings/useSlippage'
@@ -48,7 +48,7 @@ export default function useHealthComputer(account?: Account) {
   const whitelistedAssets = useWhitelistedAssets()
   const perpsAssets = usePerpsEnabledAssets()
   const { data: assetParams } = useAssetParams()
-  const { data: perpsMarketStates } = useAllPerpsMarketStates()
+  const { data: perpsMarketStates } = usePerpsMarketStates()
   const { data: perpsParams } = useAllPerpsParamsSC()
   const { data: vaultConfigs } = useVaultConfigs()
   const { data: perpsVault } = usePerpsVault()
