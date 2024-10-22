fb5dd81d5a8702f78ac1f9d48fed0aea77617758
Patricie <patricie.sm@gmail.com>
2024-10-22 17:24:47 +1300

    feat: vault details withdraw table

--- src/components/vaults/community/createVault/MintVaultAccount.tsx
+++ src/components/vaults/community/createVault/MintVaultAccount.tsx
@@ -2,8 +2,41 @@ import Button from 'components/common/Button'
 import CreateVaultContent from 'components/vaults/community/createVault/CreateVaultContent'
 import Text from 'components/common/Text'
 import { ArrowRight, Vault } from 'components/common/Icons'
+import { useCallback } from 'react'
+import useAccountId from 'hooks/accounts/useAccountId'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import useStore from 'store'
+import { getPage, getRoute } from 'utils/route'
+import VaultDetails from 'components/vaults/community/vaultDetails/index'
 
 export default function MintVaultAccount() {
+  const accountId = useAccountId()
+  const { pathname } = useLocation()
+  const [searchParams] = useSearchParams()
+  const address = useStore((s) => s.address)
+
+  const navigate = useNavigate()
+  const handleCreate = useCallback(() => {
+    // TODO: Implement mint vault acc logic
+
+    // temp vault address
+    const tempVaultAddress = 'tempvaultaddress'
+
+    if (accountId)
+      navigate(
+        getRoute(getPage(`vaults/${tempVaultAddress}/details`), searchParams, address, accountId),
+      )
+
+    useStore.setState({
+      focusComponent: {
+        component: <VaultDetails />,
+        onClose: () => {
+          navigate(getRoute(getPage(pathname), searchParams, address))
+        },
+      },
+    })
+  }, [navigate, pathname, searchParams, address, accountId])
+
   return (
     <CreateVaultContent cardClassName='h-118 flex justify-end'>
       <div className='flex flex-col items-center w-full gap-24'>
@@ -21,7 +54,7 @@ export default function MintVaultAccount() {
 
         <Button
           // TODO: add Create vault acc logic
-          onClick={() => {}}
+          onClick={handleCreate}
           variant='solid'
           color='primary'
           size='md'
