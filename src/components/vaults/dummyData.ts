275aca4fa960cd0c18178831b53a64d369dc95d7
Patricie <patricie.sm@gmail.com>
2024-10-23 20:21:22 +1300

    feat: first demo of profile card

--- src/components/vaults/dummyData.ts
+++ src/components/vaults/dummyData.ts
@@ -137,3 +137,15 @@ export const vaultBalanceData = [
     },
   },
 ]
+
+export const vaultProfileData = {
+  vaultName: 'Pepe Vault',
+  apr: 48.5,
+  tvl: 902274.47,
+  accuredPnl: 202.45,
+  wallet: 'Stargaze',
+  description:
+    "Welcome to the 'Get Rich or Die Tryin' crypto vault, where your USDC is about to hustle harder than 50 Cent in the early 2000s! I'm the fund manager, your crypto Dr. Dre, here to produce those sweet gains and turn your digital dollars into a symphony of wealth.",
+  avatarUrl:
+    'https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+}
