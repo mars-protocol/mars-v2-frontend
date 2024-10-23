2c430753e10492bc33468ad4634f92ee180fabb2
Linkie Link <linkielink.dev@gmail.com>
2024-10-23 09:36:35 +0200

    fix: added greaterThan logic to perps balances table

--- src/components/perps/BalancesTable/Columns/Leverage.tsx
+++ src/components/perps/BalancesTable/Columns/Leverage.tsx
@@ -1,6 +1,7 @@
 import { FormattedNumber } from 'components/common/FormattedNumber'
 import Text from 'components/common/Text'
 import TitleAndSubCell from 'components/common/TitleAndSubCell'
+import { formatValue } from 'utils/formatters'
 
 export const LEVERAGE_META = {
   accessorKey: 'leverage',
@@ -21,9 +22,13 @@ type Props = {
 }
 
 export default function Leverage(props: Props) {
+  const liqPrice = props.liquidationPrice.isGreaterThan(10)
+    ? formatValue(props.liquidationPrice.toNumber(), { maxDecimals: 2, abbreviated: false })
+    : formatValue(props.liquidationPrice.toNumber(), { maxDecimals: 6, abbreviated: false })
+
   return (
     <TitleAndSubCell
-      title={props.liquidationPrice.toString() ?? '-'}
+      title={liqPrice.toString() ?? '-'}
       sub={
         props.leverage ? <FormattedNumber amount={props.leverage} options={{ suffix: 'x' }} /> : ''
       }
