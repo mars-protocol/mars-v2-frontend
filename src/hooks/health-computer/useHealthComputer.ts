11e91df5dda3ca94f71011921c49d55331fdfc02
Monkmansteve <sxatzakis@yahoo.gr>
2024-11-09 02:56:32 +0200

    feature: update the prices used in oracle price

--- src/hooks/health-computer/useHealthComputer.ts
+++ src/hooks/health-computer/useHealthComputer.ts
@@ -111,6 +111,24 @@ export default function useHealthComputer(account?: Account) {
     )
   }, [assets, perpsAssets, whitelistedAssets])
 
+  const spotPriceData = useMemo(() => {
+    const assetsWithPrice = assets.filter((asset) => asset.price)
+    const prices = assetsWithPrice.map((asset) => asset.price) as BNCoin[]
+    return prices.reduce(
+      (prev, curr) => {
+        const decimals = assets.find(byDenom(curr.denom))?.decimals || PRICE_ORACLE_DECIMALS
+        const decimalDiffrence = decimals - PRICE_ORACLE_DECIMALS
+
+        prev[curr.denom] = curr.amount
+          .shiftedBy(VALUE_SCALE_FACTOR - decimalDiffrence)
+          .decimalPlaces(18)
+          .toString()
+        return prev
+      },
+      {} as { [key: string]: string },
+    )
+  }, [assets])
+
   const assetsParams = useMemo(
     () =>
       assetParams.reduce(
@@ -240,10 +258,16 @@ export default function useHealthComputer(account?: Account) {
   const computeMaxSwapAmount = useCallback(
     (from: string, to: string, kind: SwapKind, isRepayDebt: boolean) => {
       if (!healthComputer) return BN_ZERO
+
+      const swapHealthComputer = {
+        ...healthComputer,
+        oracle_prices: spotPriceData,
+      }
+
       try {
         return BN(
           max_swap_estimate_js(
-            healthComputer,
+            swapHealthComputer,
             from,
             to,
             kind,
@@ -256,7 +280,7 @@ export default function useHealthComputer(account?: Account) {
         return BN_ZERO
       }
     },
-    [healthComputer, slippage],
+    [healthComputer, slippage, spotPriceData],
   )
 
   const computeLiquidationPrice = useCallback(
--- src/utils/health_computer/index.d.ts
+++ src/utils/health_computer/index.d.ts
@@ -136,10 +136,10 @@ export interface InitOutput {
     h: number,
     i: number,
   ) => void
+  readonly interface_version_8: () => void
   readonly allocate: (a: number) => number
   readonly deallocate: (a: number) => void
   readonly requires_iterator: () => void
-  readonly interface_version_8: () => void
   readonly __wbindgen_malloc: (a: number, b: number) => number
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number
