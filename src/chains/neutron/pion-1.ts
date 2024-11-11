ac9fab12cde3d46b3e014e1951efbdc70c5f786e
Linkie Link <linkielink.dev@gmail.com>
2024-11-11 11:30:45 +0100

    fix: stable

--- src/chains/neutron/pion-1.ts
+++ src/chains/neutron/pion-1.ts
@@ -6,11 +6,11 @@ const Pion1: ChainConfig = {
   id: ChainInfoID.Pion1,
   isOsmosis: false,
   name: 'Neutron Testnet',
-  stables: ['factory/neutron1ke0vqqzyymlp5esr8gjwuzh94ysnpvj8er5hm7/USDC'],
+  stables: ['factory/neutron1ke0vqqzyymlp5esr8gjwuzh94ysnpvj8er5hm7/UUSDC'],
   campaignAssets: [],
   defaultTradingPair: {
     buy: 'untrn',
-    sell: 'factory/neutron1ke0vqqzyymlp5esr8gjwuzh94ysnpvj8er5hm7/USDC',
+    sell: 'factory/neutron1ke0vqqzyymlp5esr8gjwuzh94ysnpvj8er5hm7/UUSDC',
   },
   contracts: {
     redBank: 'neutron1vxvqs075q7y9tvpmt60xjmejrfvuw8mcvgqkug67des96y7psalqrl4cn6',
