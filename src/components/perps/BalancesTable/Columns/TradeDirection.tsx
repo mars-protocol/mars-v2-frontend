9b5ab7272fe3c5a2e91039f566c396806e05cc34
Linkie Link <linkielink.dev@gmail.com>
2024-11-14 15:04:26 +0100

    fix: added ‘Close Position’ to the direction logic

--- src/components/perps/BalancesTable/Columns/TradeDirection.tsx
+++ src/components/perps/BalancesTable/Columns/TradeDirection.tsx
@@ -32,20 +32,18 @@ function getPositionEffect(
   if (!currentPosition) return ''
   if (orderType === 'market') return ''
 
-  if (currentPosition.tradeDirection === orderDirection) {
-    return 'Increase Position'
-  }
-
-  if (isReduceOnly) return 'Reduce Position'
+  if (currentPosition.tradeDirection === orderDirection) return 'Increase Position'
 
   if (currentPosition.tradeDirection !== orderDirection) {
-    if (orderAmount.abs().isGreaterThan(currentPosition.amount.abs())) {
-      return 'Flip Position'
-    }
-
+    if (orderAmount.abs().isGreaterThan(currentPosition.amount.abs())) return 'Flip Position'
+    if (orderAmount.abs().isEqualTo(currentPosition.amount.abs())) return 'Close Position'
     return 'Reduce Position'
   }
 
+  if (isReduceOnly) {
+    if (orderAmount.abs().isGreaterThan(currentPosition.amount.abs())) return 'Close Position'
+    return 'Reduce Position'
+  }
   return ''
 }
 
