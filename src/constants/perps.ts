994ad9d710d37a9a458182dd1dd544105b65e52e
Linkie Link <linkielink.dev@gmail.com>
2024-12-04 17:24:27 +0100

    fix: perps icons

--- src/constants/perps.ts
+++ src/constants/perps.ts
@@ -139,7 +139,7 @@ export const PERPS_ASSETS: AstroportAsset[] = [
     chainId: 'neutron-1',
     denom: 'factory/neutron166t9ww3p6flv7c86376fy0r92r88t3492xxj2h/upepe',
     symbol: 'PEPE',
-    icon: 'https://raw.githubusercontent.com/mars-protocol/mars-v2-frontend/refs/heads/perps/src/components/common/Icons/PEPELogo.svg',
+    icon: 'https://raw.githubusercontent.com/mars-protocol/mars-v2-frontend/refs/heads/main/src/components/common/Icons/PEPELogo.svg',
     description: 'Pepe Coin',
     decimals: 6,
     priceUSD: 0,
