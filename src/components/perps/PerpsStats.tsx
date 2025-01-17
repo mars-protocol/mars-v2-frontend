2ea0aba6f88efd9a4d69409d204960a552e1e44b
Linkie Link <linkielink.dev@gmail.com>
2025-01-17 12:09:08 +0100

    fix: use https

--- src/components/perps/PerpsStats.tsx
+++ src/components/perps/PerpsStats.tsx
@@ -17,7 +17,7 @@ export function PerpsStats({ denom }: Props) {
     <div className='flex gap-4 h-[500px] md:h-screen/70 md:max-h-[980px] md:min-h-[560px] w-full items-center justify-center'>
       {isLoading && <CircularProgress size={60} />}
       <iframe
-        src={`http://stats.marsprotocol.io/${denom}?iframeView=on`}
+        src={`https://stats.marsprotocol.io/${denom}?iframeView=on`}
         className={classNames(
           'w-[calc(100%-2px)] ml-[1px] h-full border-0',
           isLoading ? 'hidden' : 'flex',
