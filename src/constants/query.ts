b0915e0f820eadbdf2406f009c108265569eec93
Linkie Link <linkielink.dev@gmail.com>
2025-01-28 12:24:04 +0100

    fix: reduce item limit per query to 4

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -1,4 +1,4 @@
-export const ITEM_LIMIT_PER_QUERY = 5
+export const ITEM_LIMIT_PER_QUERY = 4
 export const PRICE_ORACLE_DECIMALS = 6
 export const PRICE_REFRESH_INTERVAL = 5_000
 export const FETCH_TIMEOUT = 6_000
