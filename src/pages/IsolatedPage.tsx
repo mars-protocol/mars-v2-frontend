ad5fe0b6a913c355112bcd4c6c68c902702d4446
Patricie29 <patricie@marsprotocol.foundation>
2025-03-03 12:12:48 +0900

    tidy: remove markets from perps chart

--- src/pages/IsolatedPage.tsx
+++ src/pages/IsolatedPage.tsx
@@ -1,14 +1,17 @@
+import Card from 'components/common/Card'
 import { PerpsModule } from 'components/perps/Module/PerpsModule'
 import PerpsBanner from 'components/perps/PerpsBanner'
+import { PerpsChart } from 'components/perps/PerpsChart'
 import { PerpsPositions } from 'components/perps/PerpsPositions'
-import { PerpsTabs } from 'components/perps/PerpsTabs'
 
 export default function IsolatedPage() {
   return (
     <div className='flex flex-wrap w-full gap-4 md:grid md:grid-cols-chart'>
       <div className='w-full'>
         <PerpsBanner />
-        <PerpsTabs />
+        <Card title='Trading Chart'>
+          <PerpsChart />,
+        </Card>
       </div>
       <div className='w-full row-span-2'>
         <PerpsModule />
