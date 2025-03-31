31694fd91200c288c1a4008edaae198dc3cebc88
Patricie29 <patricie@marsprotocol.foundation>
2025-03-31 13:26:06 +0700

    refactor: deposited vault table into card tab

--- src/components/managedVaults/community/table/AvailableCommunityVaults.tsx
+++ src/components/managedVaults/community/table/AvailableCommunityVaults.tsx
@@ -1,19 +1,53 @@
+import { CardWithTabs } from 'components/common/Card/CardWithTabs'
 import { CircularProgress } from 'components/common/CircularProgress'
 import Table from 'components/common/Table'
 import Text from 'components/common/Text'
 import useCommunityVaultsColumns from 'components/managedVaults/community/table/useCommunityVaultsColumns'
 import useManagedVaults from 'hooks/managedVaults/useManagedVaults'
+import { useMemo } from 'react'
 import useStore from 'store'
 
 interface Props {
   title: string
   data: ManagedVaultsData[]
   isLoading: boolean
+  hideCard?: boolean
 }
 
 export default function AvailableCommunityVaults() {
   const { data, isLoading } = useManagedVaults()
   const address = useStore((s) => s.address)
+
+  const userTabs = useMemo(() => {
+    const tabs = []
+    if (data.ownedVaults.length > 0) {
+      tabs.push({
+        title: 'My Vaults',
+        renderContent: () => (
+          <VaultTable
+            title='My Vaults'
+            data={data.ownedVaults}
+            isLoading={isLoading}
+            hideCard={true}
+          />
+        ),
+      })
+    }
+    if (data.depositedVaults.length > 0) {
+      tabs.push({
+        title: 'My Deposits',
+        renderContent: () => (
+          <VaultTable
+            title='My Deposits'
+            data={data.depositedVaults}
+            isLoading={isLoading}
+            hideCard={true}
+          />
+        ),
+      })
+    }
+    return tabs
+  }, [data.depositedVaults, data.ownedVaults, isLoading])
 
   if (isLoading) {
     return (
@@ -35,18 +69,13 @@ export default function AvailableCommunityVaults() {
 
   return (
     <>
-      {address && (
-        <>
-          <VaultTable title='My Vaults' data={data.ownedVaults} isLoading={isLoading} />
-          <VaultTable title='My Deposits' data={data.depositedVaults} isLoading={isLoading} />
-        </>
-      )}
+      {address && <CardWithTabs tabs={userTabs} />}
       <VaultTable title='Available Vaults' data={data.availableVaults} isLoading={isLoading} />
     </>
   )
 }
 
-function VaultTable({ title, data, isLoading }: Props) {
+function VaultTable({ title, data, isLoading, hideCard }: Props) {
   const columns = useCommunityVaultsColumns({
     isLoading,
     showPosition: title === 'My Deposits',
@@ -54,5 +83,7 @@ function VaultTable({ title, data, isLoading }: Props) {
 
   if (!data.length) return null
 
-  return <Table title={title} columns={columns} data={data} initialSorting={[]} />
+  return (
+    <Table title={title} columns={columns} data={data} initialSorting={[]} hideCard={hideCard} />
+  )
 }
