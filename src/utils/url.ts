c1b868a86f1c4c9c19566365fccf4726acbc4d07
Monkmansteve <sxatzakis@yahoo.gr>
2025-05-26 15:58:16 +0300

    refactor: remove rb_asset_apr API endpoint and update URL utility to handle API key securely

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,6 +3,11 @@ export const getUrl = (baseUrl: string, path: string = ''): string => {
 
   if (isPlaceholder) return baseUrl + '/' + path
 
-  const url = new URL(path, baseUrl)
+  let url = new URL(baseUrl)
+  if (path !== '') url = new URL(path, url)
+
+  if (process.env.NEXT_PUBLIC_API_KEY)
+    url.searchParams.append('x-apikey', process.env.NEXT_PUBLIC_API_KEY)
+
   return url.toString()
 }
