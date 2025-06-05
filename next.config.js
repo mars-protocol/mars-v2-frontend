50fb2e8c4a67090744b06caaaee579b47b50327b
Linkie Link <linkielink.dev@gmail.com>
2025-06-05 15:40:22 +0200

    env: added no-cache rules to SSR

--- next.config.js
+++ next.config.js
@@ -55,6 +55,20 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
         destination: '/',
         has: [
           {
