3164aa3f149c440117d0a305492f810ea0cdb244
Linkie Link <linkielink.dev@gmail.com>
2025-06-07 15:05:53 +0200

    fix: number formatting

--- src/pages/api/og/[[...slug]].tsx
+++ src/pages/api/og/[[...slug]].tsx
@@ -1,7 +1,8 @@
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 import { getManagedVaultsUrl } from 'utils/api'
-import { formatValue } from 'utils/edgeFormatters'
+import { formatValue } from 'utils/formatters'
+import { convertAprToApy } from 'utils/parsers'
 
 export const config = {
   runtime: 'edge',
@@ -40,8 +41,15 @@ export default async function handler(req: NextRequest) {
 
     // Convert APR to APY with daily compounding
     const apr = Number(vaultInfo.apr)
-    const apy = apr ? ((1 + apr / 36500) ** 365 - 1) * 100 : null
-    const formattedAPY = apy ? `${apy.toFixed(2)}%` : '0.00%*'
+    const apy = convertAprToApy(apr, 365)
+    const formattedAPY = apy
+      ? formatValue(apy, {
+          abbreviated: false,
+          minDecimals: apy > 100 ? 0 : 2,
+          maxDecimals: apy > 100 ? 0 : 2,
+          suffix: '%',
+        })
+      : '0.00%*'
 
     const showApyNote = !apy
 
