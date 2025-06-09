db333c1a309e29a2b060897796fe7b66079def1d
Linkie Link <linkielink.dev@gmail.com>
2025-06-09 12:49:39 +0200

    env: add dynamic host

--- src/pages/api/ssr/vaults/[vaultAddress].ts
+++ src/pages/api/ssr/vaults/[vaultAddress].ts
@@ -20,6 +20,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       vault.description ||
       "Trade spot, margin and perps, lend, and earn on the Cosmos' most powerful credit protocol."
 
+    const host = req.rawHeaders[req.rawHeaders.length - 1]
+
     const html = `
       <!DOCTYPE html>
       <html lang="en">
@@ -27,13 +29,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0" />
           <title>${title}</title>
-          <meta property="og:url" content="https://staging.marsprotocol.io/vaults/${vault.vault_address}/details" />
+          <meta property="og:url" content="https://${host}/vaults/${vault.vault_address}/details" />
           <meta property="og:title" content="${title}" />
           <meta property="og:description" content="${description}" />
-          <meta property="og:image" content="https://staging.marsprotocol.io/api/og/${vault.vault_address}/${unixTime}" />
+          <meta property="og:image" content="https://${host}/api/og/${vault.vault_address}/${unixTime}" />
           <meta property="twitter:title" content="${title}" />
           <meta property="twitter:description" content="${description}" />
-          <meta property="twitter:image" content="https://staging.marsprotocol.io/api/og/${vault.vault_address}/${unixTime}" />
+          <meta property="twitter:image" content="https://${host}/api/og/${vault.vault_address}/${unixTime}" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@mars_protocol" />
           <meta name="twitter:creator" content="@mars_protocol" />
@@ -50,6 +52,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(200).send(html)
   } catch (error) {
     console.error('Error fetching vault data:', error)
-    res.status(500).json({ error: 'Internal server error' })
+    res.status(500).json({ error: 'Internal server error: ' + error })
   }
 }
