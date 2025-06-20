e825e50a6561939f7712fe51e13050e1665b077f
Patricie29 <patricie@marsprotocol.foundation>
2025-06-20 19:01:03 +0700

    refactor: unlock logic, naming for better clarity

--- src/components/managedVaults/vaultDetails/table/columns/WithdrawButton.tsx
+++ src/components/managedVaults/vaultDetails/table/columns/WithdrawButton.tsx
@@ -2,9 +2,6 @@ import Button from 'components/common/Button'
 import { AccountArrowDown } from 'components/common/Icons'
 import { useCallback, useState } from 'react'
 import useStore from 'store'
-import useCurrentWalletBalance from 'hooks/wallet/useCurrentWalletBalance'
-import { BN } from 'utils/helpers'
-import BigNumber from 'bignumber.js'
 
 export const WITHDRAW_META = {
   id: 'withdraw',
@@ -13,36 +10,25 @@ export const WITHDRAW_META = {
 }
 
 interface Props {
-  amount: string
+  totalUnlockedAmount: string
   vaultAddress: string
   vaultTokenDenom: string
   disabled: boolean
 }
 
 export default function WithdrawButton(props: Props) {
-  const { amount, vaultAddress, vaultTokenDenom, disabled } = props
-  console.log('### amount in withdraw button:', amount)
+  const { totalUnlockedAmount, vaultAddress, vaultTokenDenom, disabled } = props
 
   const address = useStore((s) => s.address)
   const [isConfirming, setIsConfirming] = useState(false)
   const withdrawFromManagedVault = useStore((s) => s.withdrawFromManagedVault)
-  const walletBalance = useCurrentWalletBalance(vaultTokenDenom)
 
   const handleWithdraw = useCallback(async () => {
     setIsConfirming(true)
     try {
-      // Ensure the withdrawal amount never exceeds the user's wallet balance
-      const walletBalanceAmount = BN(walletBalance?.amount || '0')
-      console.log('### walletBalanceAmount:', walletBalanceAmount.toString())
-      const requestedAmount = BN(amount)
-      console.log('### requestedAmount:', requestedAmount.toString())
-      const actualAmount = BigNumber.minimum(requestedAmount, walletBalanceAmount).toString()
-      console.log('### actualAmount sent to contract:', actualAmount.toString())
-      console.log('### vaultTokenDenom sent to contract:', vaultTokenDenom)
-
       await withdrawFromManagedVault({
         vaultAddress,
-        amount: actualAmount,
+        amount: totalUnlockedAmount,
         vaultToken: vaultTokenDenom,
         recipient: address,
       })
@@ -51,14 +37,7 @@ export default function WithdrawButton(props: Props) {
     } finally {
       setIsConfirming(false)
     }
-  }, [
-    address,
-    amount,
-    vaultAddress,
-    vaultTokenDenom,
-    withdrawFromManagedVault,
-    walletBalance?.amount,
-  ])
+  }, [address, totalUnlockedAmount, vaultAddress, vaultTokenDenom, withdrawFromManagedVault])
 
   return (
     <Button
